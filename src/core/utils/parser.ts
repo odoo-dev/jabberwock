7e06200dd4b6d612f943f674bc47d3b218627e2e
Antoine Guenet <age@odoo.com>
2019-08-28 11:13:02 +0200

    fix parser

--- src/core/utils/parser.ts
+++ src/core/utils/parser.ts
@@ -1,57 +1,61 @@
 import { VNode, VNodeType } from '../stores/VNode';
 import utils from './utils';
 
-const parser = {
-    /**
-     * Parse a DOM fragment or its representation as a string.
-     */
-    parse: (fragment: DocumentFragment): VNode [] => {
-        let parsedNodes: VNode [] = [];
-        const contents: NodeListOf<ChildNode> = fragment.childNodes;
-        const children: Element [] = utils._collectionToArray(contents);
-        children.forEach(child => {
-            let parsedChildren: VNode [] = this._parseOne(child);
-            parsedNodes = parsedNodes.concat(parsedChildren);
-        });
-        return parsedNodes;
-    },
-    _getNodeType: (node: Element): VNodeType => {
-        switch (node.tagName) {
-            case 'P':
-                return VNodeType.PARAGRAPH;
-            case 'H1':
-            case 'H2':
-            case 'H3':
-            case 'H4':
-            case 'H5':
-            case 'H6':
-                return VNodeType.HEADER;
-        }
-    },
-    /**
-     * Parse a DOM fragment or its representation as a string.
-     */
-    _parseOne: (node: Element): VNode [] => {
-        let parsedNode: VNode;
-        if (!node.tagName) { // node is a textNode
-            let parsedNodes: VNode [] = [];
-            for (let i = 0; i < node.textContent.length; i++) {
-                let char: string = node.textContent.charAt(i);
-                parsedNode = new VNode(VNodeType.CHAR, char);
-                parsedNodes.push(parsedNode);
-            }
-            return parsedNodes;
-        }
-        parsedNode = new VNode(this._getNodeType(node));
-        let children: Element [] = utils._collectionToArray(node.childNodes);
-        children.forEach(child => {
-            let parsedChildren: VNode [] = this._parseOne(child);
-            parsedChildren.forEach(parsedChild => {
-                parsedNode.append(parsedChild);
-            });
-        });
-        return [parsedNode];
+const _getNodeType = (node: Element): VNodeType => {
+    switch (node.tagName) {
+        case 'P':
+            return VNodeType.PARAGRAPH;
+        case 'H1':
+        case 'H2':
+        case 'H3':
+        case 'H4':
+        case 'H5':
+        case 'H6':
+            return VNodeType.HEADER;
     }
+};
+/**
+ * Parse a DOM fragment or its representation as a string.
+ */
+const _parseOne = (node: Element): VNode [] => {
+    let parsedNode: VNode;
+    if (!node.tagName) { // node is a textNode
+        let parsedNodes: VNode [] = [];
+        for (let i = 0; i < node.textContent.length; i++) {
+            let char: string = node.textContent.charAt(i);
+            parsedNode = new VNode(VNodeType.CHAR, char);
+            parsedNodes.push(parsedNode);
+        }
+        return parsedNodes;
+    }
+    parsedNode = new VNode(_getNodeType(node));
+    let children: Element [] = utils._collectionToArray(node.childNodes);
+    children.forEach(child => {
+        let parsedChildren: VNode [] = _parseOne(child);
+        parsedChildren.forEach(parsedChild => {
+            parsedNode.append(parsedChild);
+        });
+    });
+    return [parsedNode];
+}
+/**
+ * Parse a DOM fragment or its representation as a string.
+ */
+const parse = (fragment: DocumentFragment): VNode [] => {
+    let parsedNodes: VNode [] = [];
+    const contents: NodeListOf<ChildNode> = fragment.childNodes;
+    const children: Element [] = utils._collectionToArray(contents);
+    children.forEach(child => {
+        let parsedChildren: VNode [] = _parseOne(child);
+        parsedNodes = parsedNodes.concat(parsedChildren);
+    });
+    return parsedNodes;
+};
+
+const parser = {
+    parse: parse,
+    _getNodeType: _getNodeType,
+    _parseOne: _parseOne,
 };
 
 export default parser;
