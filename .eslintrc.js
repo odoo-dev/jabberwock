732859cf26ded2e8a29286ca1d381da58b439395
Nicolas Bayet <nicolas.bayet@gmail.com>
2019-08-30 11:04:45 +0200

    [ADD] eslint, typescript-eslint, prettier, vscode

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "parser":  "@typescript-eslint/parser",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": 6,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        "prettier",
+        "prettier/@typescript-eslint",  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        "plugin:prettier/recommended",  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    ],
+    "rules": {
+    },
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "browser": true,
+    },
+    "globals": {
+    }
+};
---
+++ src/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports =  {
+    semi:  true,
+    trailingComma:  'all',
+    singleQuote:  false,
+    printWidth:  120,
+    tabWidth: 4,
+    bracketSpacing: true,
+    arrowParens: "always",
+    quoteProps: 'preserve',
+    endOfLine: 'lf'
+};
