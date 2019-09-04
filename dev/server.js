2ded5a3732bd8725449c1fb94756c38bcd4d6a25
David Monjoie <dmo@odoo.com>
2019-09-04 10:28:22 +0200

    fix linting of dev and generated js files

--- dev/server.js
+++ dev/server.js
@@ -1,15 +1,15 @@
-"use strict";
-const finalhandler = require("finalhandler");
-const http = require("http");
-const serveStatic = require("serve-static");
+'use strict';
+const finalhandler = require('finalhandler');
+const http = require('http');
+const serveStatic = require('serve-static');
 
 // Serve up build folder
-const index = process.argv.length > 2 ? process.argv[2] : "index.html";
-const serve = serveStatic(".", { index: [index] });
+const index = process.argv.length > 2 ? process.argv[2] : 'index.html';
+const serve = serveStatic('.', { index: [index] });
 
 // Create server
 const server = http.createServer(function onRequest(req, res) {
-  serve(req, res, finalhandler(req, res));
+    serve(req, res, finalhandler(req, res));
 });
 
 // Listen
