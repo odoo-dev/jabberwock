7066693f8cd75f79d2db0fa9c69727379f44816a
Antoine Guenet <age@odoo.com>
2019-09-09 15:03:25 +0200

    make devtools resizable

--- src/plugins/DevTools/components/DevToolsComponent.ts
+++ src/plugins/DevTools/components/DevToolsComponent.ts
@@ -1,5 +1,4 @@
 import { Component } from '../../../../lib/owl/dist/owl.js';
-import { DOMElement } from '../../../core/types/DOMElement.js';
 import { InfoComponent } from './InfoComponent.js';
 import { PathComponent } from './PathComponent.js';
 import { TreeComponent } from './TreeComponent.js';
@@ -8,6 +7,8 @@ import { VNode } from '../../../core/stores/VNode.js';
 ////////////////////////////// todo: use API ///////////////////////////////////
 
 interface DevToolsState {
+    closed: boolean;
+    height: number; // in px
     selectedNode: VNode;
     selectedPath: VNode[];
 }
@@ -15,36 +16,44 @@ interface DevToolsState {
 export class DevToolsComponent extends Component<any, any, DevToolsState> {
     components = { InfoComponent, PathComponent, TreeComponent };
     state: DevToolsState = {
+        closed: true,
+        height: 300,
         selectedNode: this.env.editor.vDocument.contents,
         selectedPath: this._getPath(this.env.editor.vDocument.contents),
     };
     template = 'devtools';
+    _heightOnLastMousedown = this.state.height;
 
     //--------------------------------------------------------------------------
     // Public
     //--------------------------------------------------------------------------
 
+    startResize(event: MouseEvent): void {
+        if (this.state.closed) {
+            return;
+        }
+        const startHeight = this.state.height;
+        this._heightOnLastMousedown = this.state.height;
+        const resizePageY = event.pageY;
+        const doResize = (event: MouseEvent): void => {
+            this.state.height = startHeight + resizePageY - event.pageY;
+        };
+        const stopResize = (): void => {
+            window.removeEventListener('mousemove', doResize, false);
+            window.removeEventListener('mouseup', stopResize, false);
+        };
+        window.addEventListener('mousemove', doResize);
+        window.addEventListener('mouseup', stopResize);
+    }
     selectNode(event: SelectedNodeEvent): void {
         const vNode: VNode = event.detail.vNode;
         this.state.selectedNode = vNode;
         this.state.selectedPath = this._getPath(vNode);
     }
-    toggleClass(element: Element, className: string): void {
-        const currentClass: string = element.getAttribute('class') || '';
-        if (currentClass.indexOf(className) !== -1) {
-            const regex = new RegExp('\\s*' + className + '\\s*');
-            element.setAttribute('class', currentClass.replace(regex, ''));
-        } else {
-            element.setAttribute('class', currentClass + ' ' + className);
-        }
-    }
-    toggleOpen(event: Event): void {
-        let target = event.target as DOMElement;
-        while (target && !this._isRootElement(target)) {
-            target = target.parentNode;
-        }
-        if (target) {
-            this.toggleClass(target, 'closed');
+    toggleOpen(): void {
+        const didJustResize = this._heightOnLastMousedown === this.state.height;
+        if (didJustResize) {
+            this.state.closed = !this.state.closed;
         }
     }
 
@@ -61,55 +70,4 @@ export class DevToolsComponent extends Component<any, any, DevToolsState> {
         }
         return path;
     }
-    _isRootElement(element: Element): boolean {
-        return element.tagName === 'JW-DEVTOOLS';
-    }
-
-    // constructor (env) {
-    //     super(env);
-    //     // this._updateInspectedNode(this.env.editor.vDocument.contents);
-
-    // }
-
-    // _addChildren (node: VNode): Element {
-    //     let children: VNode [] = node.children.slice();
-    //     let container: Element = document.createElement('ol');
-    //     document.createElement('li').appendChild(container);
-    //     while (children.length) {
-    //         const child: VNode = children.shift();
-    //         container.appendChild(this._addOne(child));
-    //         if (child.children.length) {
-    //             container.appendChild(this._addChildren(child));
-    //         }
-    //     }
-    //     return container;
-    // }
-    // _addOne (node: VNode): Element {
-    //     let li: Element = document.createElement('li');
-    //     let contents: string = node.type;
-    //     if (node.type === VNodeType.CHAR) {
-    //         contents += ': "' + node.value + '"';
-    //     }
-    //     li.appendChild(document.createTextNode(contents));
-    //     return li;
-    // }
-    // _getTypeInfo (node: VNode): string {
-    //     if (!node.type) {
-    //         return 'Unknown node type';
-    //     }
-    //     if (node.type === VNodeType.CHAR) {
-    //         return node.type + ': "' + node.value + '"';
-    //     }
-    //     let typeInfo: string = node.type.toLowerCase().replace('_', ' ');
-    //     return typeInfo.charAt(0).toUpperCase() + typeInfo.substring(1);
-    // }
-    // _updateInspectedNode (node: VNode) {
-    //     this.state.currentNode = node;
-    //     let typeContainer: Element = this.ui.about.querySelector('.type');
-    //     typeContainer.innerHTML = this._getTypeInfo(node);
-    //     this.ui.properties.innerHTML = '<div>Length: ' + node.length + '</div>';
-    //     if (node.parent) {
-    //         this.ui.properties.innerHTML += '<div>Parent: ' + node.parent.type + '</div>';
-    //     }
-    // }
 }
