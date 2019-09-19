5a5c05c5fc413ce81274c6856bf4486c5dd87ede
Nicolas Bayet <nicolas.bayet@gmail.com>
2019-09-19 10:29:14 +0200

    [IMP] Use karma for tests
    We use Karma instead of jest because:
    - We want to test the input events with differents browsers instead
      of a "simulated environment".
    - Karma will allow us to test on many differents environments (
      chrome, firefox, browserstack, ...)

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
