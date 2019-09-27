odoo-dev/jabberwock

commit 1563bce9f0f0b884c94119467ae72620de205c40

[ADD] OwlUI: add localStorage state storing
This commits add a localStorage property to OwlUI components.

State items whose names are listed in the localStorage property
will be read from the localStorage during the willStart of the
component, and wrote to the localStorage when the state changes.

The storage key is constructed in the following way:
OwlUI{name_of_the_class_extending_OwlUIComponent}:{state_item_name}

The _readState and _writeState functions take the storage in
params, so they could be used for sessionStorage later if needed.

src/plugins/DevTools/components/DevToolsComponent.ts
@@ -1,7 +1,6 @@
 import { ActionsComponent } from './ActionsComponent';
-import { Component } from 'owl-framework';
-import { Env } from 'owl-framework/src/component/component';
 import { InspectorComponent } from './InspectorComponent';
+import { OwlUIComponent } from '../../../ui/OwlUIComponent';
 
 ////////////////////////////// todo: use API ///////////////////////////////////
 
@@ -11,7 +10,7 @@ interface DevToolsState {
     openTab: 'inspector' | 'actions';
 }
 
-export class DevToolsComponent extends Component<Env, {}, DevToolsState> {
+export class DevToolsComponent extends OwlUIComponent<{}, DevToolsState> {
     static components = { ActionsComponent, InspectorComponent };
     state: DevToolsState = {
         closed: false,

src/ui/OwlUIComponent.ts
@@ -1,0 +1,81 @@
+import { Component } from 'owl-framework';
+import { Env } from 'owl-framework/src/component/component';
+
+export class OwlUIComponent<Props, State> extends Component<Env, Props, State> {
+    _storageKeyPrefix = 'OwlUI' + this.constructor.name + ':';
+    localStorage: string[] = [];
+
+    /**
+     * Owl hook called exactly once before the initial rendering.
+     */
+    willStart(): Promise<void> {
+        this._readState(localStorage, this.localStorage);
+        return super.willStart();
+    }
+
+    /**
+     * Called by the Owl state observer every time the state changes.
+     *
+     * @param force see Owl Component
+     */
+    render(force = false): Promise<void> {
+        this._writeState(localStorage, this.localStorage);
+        return super.render(force);
+    }
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * Read the given storable state items from the given storage.
+     *
+     * @param storage from which to read the state items
+     * @param storableItems names of the state items to read from storage
+     */
+    _readState(storage: Storage, storableItems: string[]): void {
+        storableItems.forEach(itemName => {
+            const storageKey = this._storageKeyPrefix + itemName;
+            const value = storage.getItem(storageKey);
+
+            // The value of items that were not yet set in storage is null.
+            if (value !== null) {
+                // Otherwise, the value was stored as a string in the storage.
+                // Convert it to the type of the default value for the state.
+                try {
+                    this.state[itemName] = JSON.parse(value);
+                } catch (e) {
+                    // Stored item is not parseable. Keep the default value.
+                    console.warn(
+                        `Storage: Ignoring state.${itemName} stored value.\n` +
+                            `${e.name}: ${e.message}\n` +
+                            `Stored value: ${value}`,
+                    );
+                }
+            }
+        });
+    }
+
+    /**
+     * write the given storable state items to the given storage.
+     *
+     * @param storage to write the state items to
+     * @param storableItems names of the state items to write to storage
+     */
+    _writeState(storage: Storage, storableItems: string[]): void {
+        storableItems.forEach(itemName => {
+            const storageKey = this._storageKeyPrefix + itemName;
+            // Storage require items to be stored as strings.
+            try {
+                const serializedValue = JSON.stringify(this.state[itemName]);
+                storage.setItem(storageKey, serializedValue);
+            } catch (e) {
+                // State item is not serializable. Skip storing it.
+                console.warn(
+                    `Storage: Unserializable state.${itemName} value.\n` +
+                        `${e.name}: ${e.message}`,
+                );
+            }
+        });
+    }
+}

commit 5865521016aea49bcb4e62a0e75c20f3baa450e0

[FIX] DevTools: fix wrong class extension
Commit 4b39534f2 wrongly reverted the class extension change
introduced by 1563bce9f.

DevToolsComponent must extend OwlUIComponent to use localStorage.

This commit also fixes the missing doc for the localStorage attribute.

As my father would say:
"Il n'y a que ceux qui ne font rien qui ne peuvent pas se tromper."

src/plugins/DevTools/components/DevToolsComponent.ts
@@ -1,7 +1,6 @@
 import { ActionsComponent } from './ActionsComponent';
-import { Component } from 'owl-framework';
-import { Env } from 'owl-framework/src/component/component';
 import { InspectorComponent } from './InspectorComponent';
+import { OwlUIComponent } from '../../../ui/OwlUIComponent';
 
 ////////////////////////////// todo: use API ///////////////////////////////////
 
@@ -11,7 +10,7 @@ interface DevToolsState {
     currentTab: string; // Name of the current tab
 }
 
-export class DevToolsComponent extends Component<Env, {}, DevToolsState> {
+export class DevToolsComponent extends OwlUIComponent<{}, DevToolsState> {
     static components = { ActionsComponent, InspectorComponent };
     static template = 'devtools';
     state = {

src/ui/OwlUIComponent.ts
@@ -3,6 +3,9 @@ import { Env } from 'owl-framework/src/component/component';
 
 export class OwlUIComponent<Props, State> extends Component<Env, Props, State> {
     _storageKeyPrefix = 'OwlUI' + this.constructor.name + ':';
+    // State items which names are listed in the localStorage property will be
+    // read from the localStorage during the willStart of the component, and
+    // wrote back to the localStorage whenever the state changes.
     localStorage: string[] = [];
 
     /**