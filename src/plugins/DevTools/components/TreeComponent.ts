9edec968672e3caf1ee74b31d9e5260c85029b37
Antoine Guenet <age@odoo.com>
2019-10-08 13:50:19 +0200

    [ADD] DevTools: show range in Tree

--- src/plugins/DevTools/components/TreeComponent.ts
+++ src/plugins/DevTools/components/TreeComponent.ts
@@ -108,6 +108,9 @@ export class TreeComponent extends OwlUIComponent<NodeProps> {
         if (node.value) {
             return utils.toUnicode(node.value);
         }
+        if (node.type && node.type.startsWith('RANGE')) {
+            return node.type.endsWith('START') ? '【' : '】';
+        }
         if (node.type) {
             return node.type.toLowerCase();
         }
