e54ae9c704889a45752e8218df6fb33a583a2d98
Antoine Guenet <age@odoo.com>
2019-10-21 09:20:29 +0200

    [ADD] DevTools: basic inspection of the range

--- src/plugins/DevTools/components/InfoComponent.ts
+++ src/plugins/DevTools/components/InfoComponent.ts
@@ -1,7 +1,32 @@
 import { VNode } from '../../../core/stores/VNode';
 import { OwlUIComponent } from '../../../ui/OwlUIComponent';
+import { VRange } from '../../../core/stores/VRange';
+import { useState } from 'owl-framework/src/hooks';
 
+interface InfoState {
+    currentTab: string;
+    range: VRange;
+}
 export class InfoComponent extends OwlUIComponent<{}> {
+    state: InfoState = useState({
+        currentTab: 'vNode',
+        range: this.env.editor.vDocument.range,
+    });
+    localStorage = ['currentTab'];
+    /**
+     * Open the tab with the given `tabName`
+     *
+     * @param {string} tabName
+     */
+    openTab(tabName: string): void {
+        this.state.currentTab = tabName;
+    }
+    get reprEnd(): string {
+        return this._repr(this.state.range.end);
+    }
+    get reprStart(): string {
+        return this._repr(this.state.range.start);
+    }
     /**
      * Trigger a 'node-selected' custom event
      * with the given `vNode` to select it
@@ -12,5 +37,18 @@ export class InfoComponent extends OwlUIComponent<{}> {
         this.trigger('node-selected', {
             vNode: vNode,
         });
+    }
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    _repr(rangeNode: VNode): string {
+        const nextSibling = rangeNode.nextSibling();
+        const prevSibling = rangeNode.previousSibling();
+        const position = nextSibling ? 'BEFORE' : 'AFTER';
+        const reference = nextSibling || prevSibling;
+        const nodeRepr = reference.value || reference.type;
+        return `${position} ${reference.id} (${nodeRepr})`;
     }
 }
