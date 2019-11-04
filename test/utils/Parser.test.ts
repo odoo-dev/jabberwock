e69a5424317b646bbd80357437e3066844c15c4f
Nicolas Bayet <nicolas.bayet@gmail.com>
2019-11-04 16:02:03 +0100

    [FIX] Parser: nested formatting nodes

--- test/utils/Parser.test.ts
+++ test/utils/Parser.test.ts
@@ -20,6 +20,40 @@ describe('utils', () => {
                 expect(p.children[2].type).to.equal(VNodeType.CHAR);
                 expect(p.children[2].value).to.equal('a');
             });
+
+            it('handles nested formatted nodes', () => {
+                const element = document.createElement('div');
+                element.innerHTML = '<p>a<i>b<b>c</b>d</i></p>';
+                const vDocument = Parser.parse(element);
+
+                expect(vDocument.root.type).to.equal(VNodeType.ROOT);
+                expect(vDocument.root.children.length).to.equal(1);
+                const p = vDocument.root.children[0];
+                expect(p.type).to.equal(VNodeType.PARAGRAPH);
+                expect(p.children.length).to.equal(6);
+                expect(p.children[0].type).to.equal(VNodeType.RANGE_START);
+                expect(p.children[1].type).to.equal(VNodeType.RANGE_END);
+                expect(p.children[2].type).to.equal(VNodeType.CHAR);
+                expect(p.children[2].value).to.equal('a');
+                expect(p.children[3].value).to.equal('b');
+                expect(p.children[3].format).to.deep.equal({
+                    bold: false,
+                    italic: true,
+                    underlined: false,
+                });
+                expect(p.children[4].value).to.equal('c');
+                expect(p.children[4].format).to.deep.equal({
+                    bold: true,
+                    italic: true,
+                    underlined: false,
+                });
+                expect(p.children[5].value).to.equal('d');
+                expect(p.children[5].format).to.deep.equal({
+                    bold: false,
+                    italic: true,
+                    underlined: false,
+                });
+            });
         });
     });
 });
