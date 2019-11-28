1450e348df228f418503987c24a19c7f12ee9873
Nicolas Bayet <nicolas.bayet@gmail.com>
2019-11-28 14:42:43 +0100

    [ADD] Indent: add indent feature
    We are still missing a convenient way to allow plugins to register
    key and context hooks in the EventManager.
    
    Co-authored-by: Nicolas Bayet <nby@odoo.com>
    Co-authored-by: David Monjoie <dmo@odoo.com>

---
+++ packages/plugin-indent/test/Indent.test.ts
@@ -0,0 +1,109 @@
+import { describePlugin } from '../../utils/src/testUtils';
+import JWEditor from '../../core/src/JWEditor';
+import { Indent, IndentParams } from '../src/Indent';
+import { withRange, VRange } from '../../core/src/VRange';
+
+const indent = (editor: JWEditor): void => editor.execCommand('indent');
+const outdent = (editor: JWEditor): void => editor.execCommand('outdent');
+
+describePlugin(Indent, testEditor => {
+    describe('indent', () => {
+        it('should indent 4 spaces tab when range is collapsed', async function() {
+            await testEditor({
+                contentBefore: '<p>[]a</p>',
+                stepFunction: indent,
+                contentAfter: '<p>&nbsp;&nbsp; &nbsp;[]a</p>',
+            });
+            await testEditor({
+                contentBefore: '<p>a[]</p>',
+                stepFunction: indent,
+                contentAfter: '<p>a &nbsp; &nbsp;[]</p>',
+            });
+            await testEditor({
+                contentBefore: '<p>a[]b</p>',
+                stepFunction: indent,
+                contentAfter: '<p>a &nbsp; &nbsp;[]b</p>',
+            });
+        });
+        it('should replace text with 4 spaces when only one parent selected', async function() {
+            await testEditor({
+                contentBefore: '<p>a[b]</p>',
+                stepFunction: indent,
+                contentAfter: '<p>a &nbsp; &nbsp;[]</p>',
+            });
+        });
+        it('should replace text with 4 spaces when selection in multiples lines', async function() {
+            await testEditor({
+                contentBefore: '<p>a[b<br/>c]d</p>',
+                stepFunction: indent,
+                contentAfter: '<p>&nbsp;&nbsp; &nbsp;a[b<br>&nbsp;&nbsp; &nbsp;c]d</p>',
+            });
+        });
+
+        it('should indent with 4 space when multiples paragraph', async function() {
+            await testEditor({
+                contentBefore: '<p>a[b</p><p>cd</p><p>e]f</p>',
+                stepFunction: indent,
+                contentAfter:
+                    '<p>&nbsp;&nbsp; &nbsp;a[b</p>' +
+                    '<p>&nbsp;&nbsp; &nbsp;cd</p>' +
+                    '<p>&nbsp;&nbsp; &nbsp;e]f</p>',
+            });
+        });
+        it('should indent with a fake range', async function() {
+            await testEditor({
+                contentBefore: 'ab<br>cd[]',
+                stepFunction: (editor: JWEditor) => {
+                    const bNode = editor.vDocument.root.next(node => node.name === 'b');
+                    const dNode = editor.vDocument.root.next(node => node.name === 'd');
+                    withRange(VRange.selecting(bNode, dNode), range => {
+                        const indentParams: IndentParams = {
+                            range: range,
+                        };
+                        editor.execCommand('indent', indentParams);
+                    });
+                },
+                contentAfter: '&nbsp;&nbsp; &nbsp;ab<br>&nbsp;&nbsp; &nbsp;cd[]',
+            });
+        });
+    });
+    describe('outdent', async function() {
+        it('should do nothing when only one parent selected', async function() {
+            await testEditor({
+                contentBefore: '<p>&nbsp;&nbsp; &nbsp;a[b]</p>',
+                stepFunction: outdent,
+                contentAfter: '<p>&nbsp;&nbsp; &nbsp;a[b]</p>',
+            });
+        });
+        it('should outdent(up to 4 spaces) when selection in multiples lines', async function() {
+            await testEditor({
+                contentBefore: '<p>&nbsp;&nbsp; &nbsp;a[b<br/>&nbsp;&nbsp; &nbsp;c]d</p>',
+                stepFunction: outdent,
+                contentAfter: '<p>a[b<br>c]d</p>',
+            });
+        });
+        it('should outdent(up to 4 spaces) when selection in multiples paragraph', async function() {
+            await testEditor({
+                contentBefore: '<p>&nbsp;&nbsp; &nbsp;a[b<br/>&nbsp;&nbsp; &nbsp;c]d</p>',
+                stepFunction: outdent,
+                contentAfter: '<p>a[b<br>c]d</p>',
+            });
+        });
+        it('should outdent with a fake range', async function() {
+            await testEditor({
+                contentBefore: '&nbsp;&nbsp;&nbsp;&nbsp;ab<br>&nbsp;&nbsp;&nbsp;&nbsp;cd[]',
+                stepFunction: (editor: JWEditor) => {
+                    const bNode = editor.vDocument.root.next(node => node.name === 'b');
+                    const dNode = editor.vDocument.root.next(node => node.name === 'd');
+                    withRange(VRange.selecting(bNode, dNode), range => {
+                        const indentParams: IndentParams = {
+                            range: range,
+                        };
+                        editor.execCommand('outdent', indentParams);
+                    });
+                },
+                contentAfter: 'ab<br>cd[]',
+            });
+        });
+    });
+});
