aa7fb9949bb3fa1f1ed7bff219554cad72a3b09b
Antoine Guenet <age@odoo.com>
2019-12-03 16:59:16 +0100

    [REF] LineBreakNode: move line break behavior to its dedicated node
    As with `CharNode`, this implies to exclusively create `LineBreakNode`s
    instead of `VNode`s with type `LINE_BREAK` when creating line breaks.

--- packages/core/test/VDocument.test.ts
+++ packages/core/test/VDocument.test.ts
@@ -1,14 +1,13 @@
 import JWEditor from '../src/JWEditor';
 import { testEditor } from '../../utils/src/testUtils';
 import { FormatParams } from '../src/CorePlugin';
-import { VNode, VNodeType } from '../src/VNodes/VNode';
+import { LineBreakNode } from '../src/VNodes/LineBreakNode';
 
 const deleteForward = (editor: JWEditor): void => editor.execCommand('deleteForward');
 const deleteBackward = (editor: JWEditor): void => editor.execCommand('deleteBackward');
 const insertParagraphBreak = (editor: JWEditor): void => editor.execCommand('insertParagraphBreak');
-const insertLineBreak = (editor: JWEditor): void => {
-    editor.execCommand('insert', { value: new VNode(VNodeType.LINE_BREAK, 'BR') });
-};
+const insertLineBreak = (editor: JWEditor): void =>
+    editor.execCommand('insert', { value: new LineBreakNode() });
 
 describe('stores', () => {
     describe('VDocument', () => {
