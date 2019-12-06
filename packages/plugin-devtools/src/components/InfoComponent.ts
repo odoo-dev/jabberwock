3b40989a9bf434e63c35cb63995548f01e9a558e
Antoine Guenet <age@odoo.com>
2019-12-06 11:09:25 +0100

    [ADD] DevTools: represent custom properties

--- packages/plugin-devtools/src/components/InfoComponent.ts
+++ packages/plugin-devtools/src/components/InfoComponent.ts
@@ -6,6 +6,7 @@ interface InfoState {
     currentTab: string;
     range: VRange;
 }
+const vNodeKeys = Object.keys(new VNode('default'));
 export class InfoComponent extends OwlUIComponent<{}> {
     state: InfoState = {
         currentTab: 'vNode',
@@ -34,6 +35,31 @@ export class InfoComponent extends OwlUIComponent<{}> {
     className(vNode: VNode): string {
         return vNode.constructor.name;
     }
+    /**
+     * Return an object representing the given VNode's public properties as
+     * alphabetically sorted pairs of key and value strings.
+     *
+     * @param vNode
+     */
+    nodeCustomProperties(vNode: VNode): { [key: string]: string }[] {
+        return Object.keys(vNode)
+            .filter(key => !key.startsWith('_') && !vNodeKeys.includes(key))
+            .sort()
+            .map(key => {
+                let value = '' + vNode[key];
+                if (typeof vNode[key] === 'object') {
+                    if (!vNode[key] || !Object.keys(vNode[key]).length) {
+                        value = '{}';
+                    } else if (vNode[key].toString === {}.toString) {
+                        value = this._objectRepr(vNode[key]);
+                    }
+                }
+                if (typeof vNode[key] === 'string') {
+                    value = '"' + value + '"';
+                }
+                return { key: key, value: value };
+            });
+    }
 
     //--------------------------------------------------------------------------
     // Private
@@ -45,5 +71,13 @@ export class InfoComponent extends OwlUIComponent<{}> {
         const position = nextSibling ? 'BEFORE' : prevSibling ? 'AFTER' : 'INSIDE';
         const reference = nextSibling || prevSibling || rangeNode.parent;
         return `${position} ${reference.id} (${reference.name})`;
+    }
+    _objectRepr(obj: object): string {
+        return Object.keys(obj)
+            .filter(key => key !== 'toString')
+            .map(key => {
+                return key + ': ' + obj[key];
+            })
+            .join('\n');
     }
 }
