ebf95dd643708ec237b08d4b78fe486072d176c1
David Monjoie <dmo@odoo.com>
2020-01-17 15:51:55 +0100

    [REF] EventManager: add proper typing to normalized events

--- packages/core/src/EventManager.ts
+++ packages/core/src/EventManager.ts
@@ -1,8 +1,16 @@
 import { EventNormalizer } from './EventNormalizer';
 import { LineBreakNode } from './VNodes/LineBreakNode';
 import JWEditor from './JWEditor';
-import { VSelectionParams, InsertTextParams } from './CorePlugin';
+import { SetSelectionParams, InsertTextParams, InsertParams } from './CorePlugin';
 import { withRange } from './VRange';
+
+interface KeydownParams {
+    key: string;
+    ctrlKey: boolean;
+    altKey: boolean;
+    shiftKey: boolean;
+    metaKey: boolean;
+}
 
 interface DomInsertTextParams {
     text: string;
@@ -33,71 +41,96 @@ export class EventManager {
      * Callback given to the normalizer.
      */
     _onNormalizedEvent(customEvent: CustomEvent): void {
+        const eventType = customEvent.type;
         const payload = customEvent.detail;
-        switch (customEvent.type) {
-            case 'enter':
-                if (customEvent.detail.shiftKey) {
-                    return this.editor.execCommand('insert', { node: new LineBreakNode() });
-                } else {
-                    return this.editor.execCommand('insertParagraphBreak');
-                }
-            case 'insertText': {
-                const params = payload as DomInsertTextParams;
-                const insertTextParams: InsertTextParams = {
-                    text: params.text,
-                };
-                if (params.range) {
-                    const rangeBounds = this.editor.parser.parseRange(params.range);
-                    return withRange(rangeBounds, range => {
-                        insertTextParams.range = range;
-                        return this.editor.execCommand(customEvent.type, insertTextParams);
-                    });
-                } else {
-                    return this.editor.execCommand(customEvent.type, insertTextParams);
-                }
-            }
-            case 'selectAll':
-            case 'setSelection': {
-                const selectionParams = payload as DomSetSelectionParams;
-                const vSelectionParams: VSelectionParams = {
-                    vSelection: this.editor.parser.parseSelection(selectionParams.domSelection),
-                };
-                return this.editor.execCommand(customEvent.type, vSelectionParams);
-            }
-            case 'keydown': {
-                // TODO: keydown should be matched with existing shortcuts. If
-                // it matches an command shortcut, trigger the corresponding
-                // command, otherwise do not trigger any command.
-                if (
-                    payload.ctrlKey &&
-                    !payload.altKey &&
-                    !payload.shiftKey &&
-                    !payload.metaKey &&
-                    payload.key === 'b'
-                ) {
-                    return this.editor.execCommand('applyFormat', { format: 'bold' });
-                } else if (
-                    payload.ctrlKey &&
-                    !payload.altKey &&
-                    !payload.shiftKey &&
-                    !payload.metaKey &&
-                    payload.key === 'i'
-                ) {
-                    return this.editor.execCommand('applyFormat', { format: 'italic' });
-                } else if (
-                    payload.ctrlKey &&
-                    !payload.altKey &&
-                    !payload.shiftKey &&
-                    !payload.metaKey &&
-                    payload.key === 'u'
-                ) {
-                    return this.editor.execCommand('applyFormat', { format: 'underline' });
-                }
-                // TODO: keydown should be matched with existing shortcuts.
-                return;
-            }
-            default:
-                this.editor.execCommand(customEvent.type, payload);
+        if (this._isKeydown(eventType, payload)) {
+            this._onKeydown(payload);
+        } else if (this._isEnter(eventType, payload)) {
+            this._onEnter(payload);
+        } else if (this._isInsertText(eventType, payload)) {
+            this._onInsertText(payload);
+        } else if (this._isSetSelection(eventType, payload)) {
+            this._onSetSelection(payload);
+        } else {
+            this.editor.execCommand(customEvent.type, payload);
         }
+    }
+
+    _isKeydown(eventType: string, payload: {}): payload is KeydownParams {
+        return payload && eventType === 'keydown';
+    }
+
+    _onKeydown(payload: KeydownParams): void {
+        // TODO: keydown should be matched with existing shortcuts.
+        if (
+            payload.ctrlKey &&
+            !payload.altKey &&
+            !payload.shiftKey &&
+            !payload.metaKey &&
+            payload.key === 'b'
+        ) {
+            this.editor.execCommand('applyFormat', { format: 'bold' });
+        } else if (
+            payload.ctrlKey &&
+            !payload.altKey &&
+            !payload.shiftKey &&
+            !payload.metaKey &&
+            payload.key === 'i'
+        ) {
+            this.editor.execCommand('applyFormat', { format: 'italic' });
+        } else if (
+            payload.ctrlKey &&
+            !payload.altKey &&
+            !payload.shiftKey &&
+            !payload.metaKey &&
+            payload.key === 'u'
+        ) {
+            this.editor.execCommand('applyFormat', { format: 'underline' });
+        }
+    }
+
+    _isEnter(eventType: string, payload: {}): payload is KeydownParams {
+        return payload && eventType === 'enter';
+    }
+
+    _onEnter(payload: KeydownParams): void {
+        if (payload.shiftKey) {
+            const params: InsertParams = {
+                node: new LineBreakNode(),
+            };
+            this.editor.execCommand('insert', params);
+        } else {
+            this.editor.execCommand('insertParagraphBreak');
+        }
+    }
+
+    _isInsertText(eventType: string, payload: {}): payload is DomInsertTextParams {
+        return payload && eventType === 'insertText';
+    }
+
+    _onInsertText(payload: DomInsertTextParams): void {
+        const params: InsertTextParams = {
+            text: payload.text,
+        };
+        if (payload.range) {
+            const boundaries = this.editor.parser.parseRange(payload.range);
+            withRange(boundaries, range => {
+                params.range = range;
+                this.editor.execCommand('insertText', params);
+            });
+        } else {
+            this.editor.execCommand('insertText', params);
+        }
+    }
+
+    _isSetSelection(eventType: string, payload: {}): payload is DomSetSelectionParams {
+        return payload && eventType === 'setSelection';
+    }
+
+    _onSetSelection(payload: DomSetSelectionParams): void {
+        const params: SetSelectionParams = {
+            ...this.editor.parser.parseSelection(payload.domSelection),
+        };
+        this.editor.execCommand('setSelection', params);
     }
 }
