b3b01525342fc9fd3e6ce2e624dce100aff433f7
Antoine Guenet <age@odoo.com>
2020-01-23 15:48:26 +0100

    [REF] Tests: move Paragraph tests to Paragraph plugin

---
+++ packages/plugin-paragraph/test/Paragraph.test.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import { ParagraphNode } from '../ParagraphNode';
+
+describe('plugin-paragraph', () => {
+    describe('ParagraphNode', () => {
+        it('should create a paragraph', async () => {
+            const vNode = new ParagraphNode();
+            expect(vNode.atomic).to.equal(false);
+            expect(vNode.htmlTag).to.equal('P');
+        });
+    });
+});
