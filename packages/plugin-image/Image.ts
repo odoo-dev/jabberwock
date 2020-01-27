93b903d1e1719b6b4656a7c526eae2e13ba400e6
Antoine Guenet <age@odoo.com>
2020-01-27 15:05:21 +0100

    [ADD] VNode: Image

---
+++ packages/plugin-image/Image.ts
@@ -0,0 +1,8 @@
+import { JWPlugin, JWPluginConfig } from '../core/src/JWPlugin';
+import { ImageDomParser } from './ImageDomParser';
+import { ImageDomRenderer } from './ImageDomRenderer';
+
+export class Image<T extends JWPluginConfig> extends JWPlugin<T> {
+    readonly parsers = [ImageDomParser];
+    readonly renderers = [ImageDomRenderer];
+}
