d66be44c9f4a2697bbef9e3ea763773e6f3569b9
Antoine Guenet <age@odoo.com>
2020-01-27 15:42:06 +0100

    [ADD] VNode: Youtube

---
+++ packages/plugin-youtube/YoutubeDomRenderer.ts
@@ -0,0 +1,15 @@
+import { AbstractRenderer } from '../core/src/AbstractRenderer';
+import { YoutubeNode } from './YoutubeNode';
+import { DomRenderingEngine } from '../plugin-dom/DomRenderingEngine';
+
+export class YoutubeDomRenderer extends AbstractRenderer<Node[]> {
+    static id = 'dom';
+    engine: DomRenderingEngine;
+    predicate = YoutubeNode;
+
+    async render(node: YoutubeNode): Promise<Node[]> {
+        const youtube = document.createElement('iframe');
+        this.engine.renderAttributes(node.attributes, youtube);
+        return [youtube];
+    }
+}
