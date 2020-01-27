61eef97e70717a691d2b8cf70a63f4708ed32455
Antoine Guenet <age@odoo.com>
2020-01-27 13:59:57 +0100

    [ADD] Format: link (a)

---
+++ packages/plugin-link/LinkDomParser.ts
@@ -0,0 +1,22 @@
+import { FormatParser } from '../plugin-inline/FormatParser';
+import { DomParsingEngine } from '../plugin-dom/DomParsingEngine';
+import { VNode } from '../core/src/VNodes/VNode';
+import { LinkFormat } from './LinkFormat';
+
+export class LinkDomParser extends FormatParser {
+    static id = 'dom';
+    engine: DomParsingEngine;
+
+    predicate = (item: Node): boolean => {
+        return item instanceof Element && item.nodeName === 'A';
+    };
+
+    async parse(item: Element): Promise<VNode[]> {
+        const link = new LinkFormat();
+        link.attributes = this.engine.parseAttributes(item);
+        const children = await this.engine.parse(...item.childNodes);
+        this.applyFormat(link, children);
+
+        return children;
+    }
+}
