93ea5a28f2308e6e4dc9358792fee2f74154720e
Antoine Guenet <age@odoo.com>
2020-02-12 17:02:55 +0100

    [FIX] LineBreak: don't ignore edge-of-inline br's if not edge-of-block
    Prior to this fix, an assumption was made that a br with no next sibling
    was invisible. This was wrong: the correct assumption is that a br at
    the end edge of a _block_ is invisible. In the example
    `<p><b>ab<br></b><br>cd</p>`, both br's are indeed visible even though
    the first one has no next sibling.

--- packages/plugin-linebreak/LineBreakDomParser.ts
+++ packages/plugin-linebreak/LineBreakDomParser.ts
@@ -1,6 +1,7 @@
 import { LineBreakNode } from './LineBreakNode';
 import { AbstractParser } from '../core/src/AbstractParser';
 import { DomParsingEngine } from '../plugin-dom/DomParsingEngine';
+import { isBlock } from '../utils/src/utils';
 
 export class LineBreakDomParser extends AbstractParser<Node> {
     static id = 'dom';
@@ -11,14 +12,38 @@ export class LineBreakDomParser extends AbstractParser<Node> {
     };
 
     async parse(item: Element): Promise<LineBreakNode[]> {
-        if (!item.nextSibling) {
-            // A <br/> with no siblings is there only to make its parent visible.
-            // Consume it since it was just parsed as its parent element node.
-            // TODO: do this less naively to account for formatting space.
+        if (!this._nextLeafInSameBlock(item)) {
+            // A <br/> at the end edge of a block is there only to make its
+            // parent visible. Consume it since it was just parsed as its parent
+            // element node. TODO: do this less naively to account for
+            // formatting space.
             return [];
         }
         const lineBreak = new LineBreakNode();
         lineBreak.attributes = this.engine.parseAttributes(item);
         return [lineBreak];
+    }
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * Return the next leaf of the given node without crossing over blocks.
+     *
+     * @param item
+     */
+    _nextLeafInSameBlock(item: Node): Node {
+        let next = item.nextSibling;
+        if (!next) {
+            return item.parentNode && this._nextLeafInSameBlock(item.parentNode);
+        } else if (isBlock(next)) {
+            return;
+        } else {
+            while (next.firstChild) {
+                next = next.firstChild;
+            }
+            return next;
+        }
     }
 }
