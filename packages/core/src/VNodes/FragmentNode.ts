800858a36db3c1fc50ebb139015fb7157efd52df
David Monjoie <dmo@odoo.com>
2020-02-16 10:23:57 +0100

    [IMP] FragmentNode: append children automatically

--- packages/core/src/VNodes/FragmentNode.ts
+++ packages/core/src/VNodes/FragmentNode.ts
@@ -2,4 +2,8 @@ import { VNode, VNodeType } from './VNode';
 
 export class FragmentNode extends VNode {
     type = VNodeType.FRAGMENT;
+
+    set parent(parent: VNode) {
+        this.mergeWith(parent);
+    }
 }
