8ac2c931dffa70bca25bd7b684e3cc81043965b4
David Monjoie <dmo@odoo.com>
2020-02-16 10:43:19 +0100

    [ADD] VRange: introduce `split`

---
+++ packages/core/test/VRange.test.ts
@@ -0,0 +1,19 @@
+import { testEditor } from '../../utils/src/testUtils';
+import { BasicEditor } from '../../../bundles/BasicEditor';
+
+describe('VRange', () => {
+    describe('split', () => {
+        it('should split the range containers in place', async function() {
+            await testEditor(BasicEditor, {
+                contentBefore: '<v>abc<w>de[f<x>ghi<y>jkl<z>mno</z>pqr</y>stu</x>vw</w>xy]z</v>',
+                stepFunction: async (editor: BasicEditor) => {
+                    const nodes = editor.vDocument.selection.range.split();
+                    editor.vDocument.root.lastChild().after(nodes[0]);
+                    await editor.execCommand('refresh');
+                },
+                contentAfter:
+                    '<v>abc<w>de</w></v><v>z</v><v><w>[f<x>ghi<y>jkl<z>mno</z>pqr</y>stu</x>vw</w>xy]</v>',
+            });
+        });
+    });
+});
