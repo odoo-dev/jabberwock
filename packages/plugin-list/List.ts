1be3d380daa2d1994cdc59a43e81409465dd289d
David Monjoie <dmo@odoo.com>
2020-02-17 15:49:01 +0100

    [IMP] List: add indent and outdent
    Co-authored-by: Nicolas Bayet <nby@odoo.com>
    Co-authored-by: David Monjoie <dmo@odoo.com>

--- packages/plugin-list/List.ts
+++ packages/plugin-list/List.ts
@@ -7,6 +7,7 @@ import { ListItemDomRenderer } from './ListItemDomRenderer';
 import { ListDomParser } from './ListDomParser';
 import { ListItemDomParser } from './ListItemDomParser';
 import { withRange, VRange } from '../core/src/VRange';
+import { IndentParams, OutdentParams } from '../plugin-indent/src/Indent';
 
 export interface ListParams extends RangeParams {
     type: ListType;
@@ -18,7 +19,18 @@ export class List extends JWPlugin {
     }
     commands = {
         toggleList: {
+            title: 'Toggle list',
             handler: this.toggleList.bind(this),
+        },
+        indent: {
+            title: 'Indent list items',
+            predicates: [ListNode],
+            handler: this.indent.bind(this),
+        },
+        outdent: {
+            title: 'Outdent list items',
+            predicates: [ListNode],
+            handler: this.outdent.bind(this),
         },
     };
     shortcuts = [
@@ -62,6 +74,108 @@ export class List extends JWPlugin {
             this._unlist(range);
         } else {
             this._convertToList(range, type);
+        }
+    }
+
+    /**
+     * Indent one or more list items.
+     *
+     * @param params
+     */
+    indent(params: IndentParams): void {
+        const range = params.range || this.editor.vDocument.selection.range;
+        const listItems = range.targetedNodes(node => node.parent && node.parent.is(ListNode));
+
+        // Filter out non-identable list items.
+        const itemsToIndent = listItems.filter(item => {
+            const nodeIndex = item.parent.children().indexOf(item);
+            if (nodeIndex === 0) {
+                // Do not indent the first list item of a list.
+                return false;
+            } else if (nodeIndex === 1 && item.is(ListNode)) {
+                // Do not indent the second item of a list if it is a nested
+                // list following a non-list item.
+                return item.previousSibling().is(ListNode);
+            } else {
+                // Do not indent items of a targeted nested list, since they
+                // will automatically be indented with their list ancestor.
+                return !listItems.includes(item.ancestor(ListNode));
+            }
+        });
+
+        if (!itemsToIndent.length) return;
+
+        // Extend indenting to the next nested list sibling of a non-list item.
+        const lastItem = itemsToIndent[itemsToIndent.length - 1];
+        if (!lastItem.is(ListNode)) {
+            const nextSibling = lastItem.nextSibling();
+            if (nextSibling && nextSibling.is(ListNode)) {
+                itemsToIndent.push(nextSibling);
+            }
+        }
+
+        for (const item of itemsToIndent) {
+            const listType = item.ancestor(ListNode).listType;
+            const prev = item.previousSibling();
+            const next = item.nextSibling();
+            // If possible, indent the item by putting it into a pre-existing
+            // list sibling of the same type.
+            if (prev && prev.is(ListNode) && prev.listType === listType) {
+                prev.append(item);
+            } else if (
+                next &&
+                next.is(ListNode) &&
+                next.listType === listType &&
+                !itemsToIndent.includes(next)
+            ) {
+                next.prepend(item);
+            } else {
+                // If no other candidate exists then wrap it in a new ListNode.
+                item.wrap(new ListNode(listType));
+            }
+        }
+    }
+
+    /**
+     * Outdent one or more list items.
+     *
+     * @param params
+     */
+    outdent(params: OutdentParams): void {
+        const range = params.range || this.editor.vDocument.selection.range;
+        const listItems = range.targetedNodes(node => node.parent && node.parent.is(ListNode));
+
+        // Do not outdent items of a targeted nested list, since they
+        // will automatically be outdented with their list ancestor.
+        const itemsToOutdent = listItems.filter(item => {
+            return !listItems.includes(item.ancestor(ListNode));
+        });
+
+        if (!itemsToOutdent.length) return;
+
+        // Extend outdenting to the next nested list sibling of a non-list item.
+        const lastItem = itemsToOutdent[itemsToOutdent.length - 1];
+        if (!lastItem.is(ListNode)) {
+            const nextSibling = lastItem.nextSibling();
+            if (nextSibling && nextSibling.is(ListNode)) {
+                itemsToOutdent.push(nextSibling);
+            }
+        }
+
+        for (const item of itemsToOutdent) {
+            const list = item.ancestor(ListNode);
+            const previousSibling = item.previousSibling();
+            const nextSibling = item.nextSibling();
+            if (previousSibling && nextSibling) {
+                const splitList = item.parent.splitAt(item);
+                splitList.before(item);
+            } else if (previousSibling) {
+                list.after(item);
+            } else if (nextSibling) {
+                list.before(item);
+            } else {
+                list.unwrap();
+            }
         }
     }
 
