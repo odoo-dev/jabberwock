736d4ef622b02d1705d3a390d442e55141972123
David Monjoie <dmo@odoo.com>
2020-03-25 15:46:43 +0100

    [FIX] Core: fix insertParagraphBreak in unbreakable

--- packages/core/src/VDocument.ts
+++ packages/core/src/VDocument.ts
@@ -1,6 +1,5 @@
 import { VNode } from './VNodes/VNode';
 import { FragmentNode } from './VNodes/FragmentNode';
-import { LineBreakNode } from '../../plugin-linebreak/src/LineBreakNode';
 
 export class VDocument {
     root: FragmentNode;
@@ -13,21 +12,6 @@ export class VDocument {
     // Public
     //--------------------------------------------------------------------------
 
-    /**
-     * Insert a paragraph break.
-     */
-    insertParagraphBreak(range): void {
-        // Remove the contents of the range if needed.
-        if (!range.isCollapsed()) {
-            range.empty();
-        }
-        if (range.startContainer.breakable) {
-            range.startContainer.splitAt(range.start);
-        } else {
-            // TODO
-            range.start.before(new LineBreakNode());
-        }
-    }
     /**
      * Insert something at range.
      *
