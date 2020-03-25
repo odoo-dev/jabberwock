7d89007be6b76ae7c19229b5a6c52a43c3479de9
David Monjoie <dmo@odoo.com>
2020-03-25 15:35:18 +0100

    [ADD] Core: introduce SeparatorNode

---
+++ packages/core/src/VNodes/SeparatorNode.ts
@@ -0,0 +1,6 @@
+import { AtomicNode } from './AtomicNode';
+
+/**
+ * This class represents an atomic node that is used as a content separator.
+ */
+export class SeparatorNode extends AtomicNode {}
