36fe9b8c1925ef3608ff08d4cba91fe2a05a5b8c
Antoine Guenet <age@odoo.com>
2020-03-30 15:24:55 +0200

    [FIX] Format: always apply a clone of a format

--- packages/plugin-inline/src/FormatParser.ts
+++ packages/plugin-inline/src/FormatParser.ts
@@ -12,11 +12,11 @@ export abstract class FormatParser extends AbstractParser<Node> {
     applyFormat(format: Format, nodes: VNode[]): void {
         for (const node of nodes) {
             if (node.is(InlineNode)) {
-                format.applyTo(node);
+                format.clone().applyTo(node);
             } else {
                 const inlineNodes = node.descendants(InlineNode);
                 for (const inline of inlineNodes) {
-                    format.applyTo(inline);
+                    format.clone().applyTo(inline);
                 }
             }
         }
