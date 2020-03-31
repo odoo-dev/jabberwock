e80d6ddb0677507e4e8ec4085e17507cc056fef0
Antoine Guenet <age@odoo.com>
2020-03-31 16:20:26 +0200

    [REF] Color: add a ColorPlugin that BackgroundColor and TextColor extend

--- packages/plugin-backgroundcolor/src/BackgroundColor.ts
+++ packages/plugin-backgroundcolor/src/BackgroundColor.ts
@@ -1,36 +1,16 @@
-import { JWPlugin, JWPluginConfig } from '../../core/src/JWPlugin';
-import { CommandParams } from '../../core/src/Dispatcher';
-import { VNode } from '../../core/src/VNodes/VNode';
+import { Color, ColorConfig } from '../../plugin-color/src/Color';
 import { Loadables } from '../../core/src/JWEditor';
 import { Keymap } from '../../plugin-keymap/src/Keymap';
-import { Inline } from '../../plugin-inline/src/Inline';
-import { InlineNode } from '../../plugin-inline/src/InlineNode';
-import { setStyle, getStyle, removeStyle } from '../../utils/src/utils';
-import { VRange } from '../../core/src/VRange';
-import { Format } from '../../plugin-inline/src/Format';
-import { FragmentNode } from '../../core/src/VNodes/FragmentNode';
 
-export interface ColorParams extends CommandParams {
-    color?: string; // css-valid color name
-}
-
-interface BackgroundColorConfig extends JWPluginConfig {
-    defaultBackgroundColor?: string;
-}
-
-const defaultConfiguration = {
-    defaultBackgroundColor: 'white',
-};
-
-export class BackgroundColor<T extends BackgroundColorConfig = BackgroundColorConfig> extends JWPlugin<T> {
-    static dependencies = [Inline];
-    configuration = { ...defaultConfiguration, ...this.configuration };
+export class BackgroundColor<T extends ColorConfig = ColorConfig> extends Color<T> {
+    styleName = 'background-color';
+    configuration = { defaultColor: 'white', ...this.configuration };
     commands = {
         colorBackground: {
-            handler: this.colorBackground,
+            handler: this.color,
         },
         uncolorBackground: {
-            handler: this.uncolorBackground,
+            handler: this.uncolor,
         },
     };
     readonly loadables: Loadables<Keymap> = {
@@ -49,148 +29,4 @@ export class BackgroundColor<T extends BackgroundColorConfig = BackgroundColorCo
             },
         ],
     };
-
-    static hasColor(node: VNode): boolean;
-    static hasColor(color: string, node: VNode): boolean;
-    static hasColor(color: string | VNode, node?: VNode): boolean {
-        if (color instanceof VNode) {
-            node = color;
-        }
-        const nodeBackgroundColor = getStyle(node, 'background-color');
-        if (color instanceof VNode) {
-            return !!nodeBackgroundColor;
-        } else {
-            return nodeBackgroundColor === color;
-        }
-    }
-
-    //--------------------------------------------------------------------------
-    // Public
-    //--------------------------------------------------------------------------
-
-    /**
-     * Apply the given background color to the range.
-     *
-     * @param params
-     */
-    colorBackground(params: ColorParams): void {
-        const color = params.color;
-        const styleName = 'background-color';
-
-        if (params.context.range.isCollapsed()) {
-            // Set the style cache.
-            const inline = this.dependencies.get(Inline);
-            const currentCache = inline.cache.style || {};
-            inline.cache.style = { ...currentCache, [styleName]: color };
-        } else {
-            for (const node of this._highestSelectedNodes(params.context.range)) {
-                // Skip if the node already has the right color, through an
-                // ancestor or a format.
-                if (node.is(InlineNode)) {
-                    const colorFormat = node.formats.find(format => getStyle(format, styleName));
-                    if (colorFormat && getStyle(colorFormat, styleName) === color) continue;
-                }
-                const colorAncestor = node.ancestor(BackgroundColor.hasColor);
-                if (colorAncestor && BackgroundColor.hasColor(color, colorAncestor)) continue;
-
-                // Apply the style to the node or its first format.
-                setStyle(this._nodeOrFirstFormat(node), styleName, color);
-
-                // If there are ancestors of this node whose children all have
-                // this style, style these ancestors instead of their
-                // descendants.
-                let parent = node.parent;
-                while (parent && !parent.test(FragmentNode) && this._isAllColored(parent, color)) {
-                    setStyle(parent, styleName, color);
-                    for (const child of parent.children()) {
-                        removeStyle(child, styleName);
-                    }
-                    parent = parent.parent;
-                }
-            }
-        }
-    }
-    /**
-     * Remove the current background color from the range. If the color was
-     * applied to an ancestor, apply the default color to its relevant inline
-     * descendants.
-     *
-     * @param params
-     */
-    uncolorBackground(params: CommandParams): void {
-        const range = params.context.range;
-        const defaultColor = this.configuration.defaultBackgroundColor;
-        const hasColor = BackgroundColor.hasColor;
-        const styleName = 'background-color';
-
-        if (range.isCollapsed()) {
-            const inline = this.dependencies.get(Inline);
-
-            if (range.start.ancestor(hasColor)) {
-                // Set the background color style cache to the default color.
-                const currentCache = inline.cache.style || {};
-                inline.cache.style = { ...currentCache, [styleName]: defaultColor };
-            } else if (inline.cache.style) {
-                // Unset the background color style cache.
-                delete inline.cache.style[styleName];
-            }
-        } else {
-            for (const node of this._highestSelectedNodes(params.context.range)) {
-                const target = this._nodeOrFirstFormat(node);
-                const currentColor = getStyle(target, styleName);
-
-                if (!currentColor || currentColor === defaultColor || node.ancestor(hasColor)) {
-                    // Set the background color to the default color.
-                    setStyle(target, styleName, defaultColor);
-                } else {
-                    // Remove the background color.
-                    removeStyle(target, styleName);
-                }
-
-                // Uncolor the children and their formats as well.
-                for (const child of node.children()) {
-                    removeStyle(child, styleName);
-                    if (child.is(InlineNode)) {
-                        for (const format of child.formats) {
-                            removeStyle(format, styleName);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    //--------------------------------------------------------------------------
-    // Private
-    //--------------------------------------------------------------------------
-
-    /**
-     * Return the range's selected nodes, filtering out a selected node's
-     * children if they are listed.
-     *
-     * @param range
-     */
-    _highestSelectedNodes(range: VRange): VNode[] {
-        const selectedNodes = range.selectedNodes();
-        return selectedNodes.filter(node => {
-            return !node.ancestor(ancestor => selectedNodes.includes(ancestor));
-        });
-    }
-    /**
-     * Return the node's first format if any, itself otherwise.
-     *
-     * @param node
-     */
-    _nodeOrFirstFormat(node: VNode): VNode | Format {
-        return node.is(InlineNode) ? node.formats[0] || node : node;
-    }
-    /**
-     * Return true if all the children of the given node have the given color.
-     *
-     * @param node
-     * @param color
-     */
-    _isAllColored(node: VNode, color: string): boolean {
-        return node.children().every(child => BackgroundColor.hasColor(color, child));
-    }
 }
---
+++ packages/plugin-color/src/Color.ts
@@ -0,0 +1,169 @@
+import { JWPlugin, JWPluginConfig } from "../../core/src/JWPlugin";
+import { CommandParams } from "../../core/src/Dispatcher";
+import { Inline } from "../../plugin-inline/src/Inline";
+import { VNode } from "../../core/src/VNodes/VNode";
+import { getStyle, setStyle, removeStyle } from "../../utils/src/utils";
+import { InlineNode } from "../../plugin-inline/src/InlineNode";
+import { FragmentNode } from "../../core/src/VNodes/FragmentNode";
+import { VRange } from "../../core/src/VRange";
+import { Format } from "../../plugin-inline/src/Format";
+
+export interface ColorParams extends CommandParams {
+    color?: string; // css-valid color name
+}
+export interface ColorConfig extends JWPluginConfig {
+    defaultColor?: string; // css-valid color name
+}
+
+export class Color<T extends ColorConfig = ColorConfig> extends JWPlugin<T> {
+    static dependencies = [Inline];
+    styleName: string; // css-valid style property name (eg: 'background-color')
+
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+
+    /**
+     * Return true if the given node has the given color.
+     * If no color is passed, return true if the given node has any color.
+     * Return false otherwise.
+     */
+    hasColor(node: VNode): boolean;
+    hasColor(color: string, node: VNode): boolean;
+    hasColor(color: string | VNode, node?: VNode): boolean {
+        if (color instanceof VNode) {
+            node = color;
+        }
+        const nodeBackgroundColor = getStyle(node, this.styleName);
+        if (color instanceof VNode) {
+            return !!nodeBackgroundColor;
+        } else {
+            return nodeBackgroundColor === color;
+        }
+    }
+    /**
+     * Apply the given color to the range.
+     *
+     * @param params
+     */
+    color(params: ColorParams): void {
+        const color = params.color;
+
+        if (params.context.range.isCollapsed()) {
+            // Set the style cache.
+            const inline = this.dependencies.get(Inline);
+            const currentCache = inline.cache.style || {};
+            inline.cache.style = { ...currentCache, [this.styleName]: color };
+        } else {
+            // TODO: apply on all `selectedNodes` when we have modifiers.
+            for (const node of this._highestSelectedNodes(params.context.range)) {
+                // Skip if the node already has the right color, through an
+                // ancestor or a format.
+                if (node.is(InlineNode)) {
+                    const colorFormat = node.formats.find(format => getStyle(format, this.styleName));
+                    if (colorFormat && getStyle(colorFormat, this.styleName) === color) continue;
+                }
+                const colorAncestor = node.ancestor(this.hasColor.bind(this));
+                if (colorAncestor && this.hasColor(color, colorAncestor)) continue;
+
+                // Apply the style to the node or its first format.
+                setStyle(this._nodeOrFirstFormat(node), this.styleName, color);
+
+                // If there are ancestors of this node whose children all have
+                // this style, style these ancestors instead of their
+                // descendants.
+                let parent = node.parent;
+                while (parent && !parent.test(FragmentNode) && this._isAllColored(parent, color)) {
+                    setStyle(parent, this.styleName, color);
+                    // TODO: not remove the children's styles when we have modifiers.
+                    for (const child of parent.children()) {
+                        removeStyle(child, this.styleName);
+                    }
+                    parent = parent.parent;
+                }
+            }
+        }
+    }
+    /**
+     * Remove the current color from the range. If the color was applied to
+     * an ancestor, apply the default color to its relevant inline descendants.
+     *
+     * @param params
+     */
+    uncolor(params: CommandParams): void {
+        const range = params.context.range;
+        const defaultColor = this.configuration.defaultColor;
+        const hasColor = this.hasColor.bind(this);
+
+        if (range.isCollapsed()) {
+            const inline = this.dependencies.get(Inline);
+
+            if (range.start.ancestor(hasColor)) {
+                // Set the color style cache to the default color.
+                const currentCache = inline.cache.style || {};
+                inline.cache.style = { ...currentCache, [this.styleName]: defaultColor };
+            } else if (inline.cache.style) {
+                // Unset the color style cache.
+                delete inline.cache.style[this.styleName];
+            }
+        } else {
+            // TODO: apply on all `selectedNodes` when we have modifiers.
+            for (const node of this._highestSelectedNodes(params.context.range)) {
+                const target = this._nodeOrFirstFormat(node);
+                const currentColor = getStyle(target, this.styleName);
+
+                if (!currentColor || currentColor === defaultColor || node.ancestor(hasColor)) {
+                    // Set the color to the default color.
+                    setStyle(target, this.styleName, defaultColor);
+                } else {
+                    // Remove the color.
+                    removeStyle(target, this.styleName);
+                }
+
+                // Uncolor the children and their formats as well.
+                for (const child of node.children()) {
+                    removeStyle(child, this.styleName);
+                    if (child.is(InlineNode)) {
+                        for (const format of child.formats) {
+                            removeStyle(format, this.styleName);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * Return the range's selected nodes, filtering out a selected node's
+     * children if they are listed.
+     *
+     * @param range
+     */
+    _highestSelectedNodes(range: VRange): VNode[] {
+        const selectedNodes = range.selectedNodes();
+        return selectedNodes.filter(node => {
+            return !node.ancestor(ancestor => selectedNodes.includes(ancestor));
+        });
+    }
+    /**
+     * Return the node's first format if any, itself otherwise.
+     *
+     * @param node
+     */
+    _nodeOrFirstFormat(node: VNode): VNode | Format {
+        return node.is(InlineNode) ? node.formats[0] || node : node;
+    }
+    /**
+     * Return true if all the children of the given node have the given color.
+     *
+     * @param node
+     * @param color
+     */
+    _isAllColored(node: VNode, color: string): boolean {
+        return node.children().every(child => this.hasColor(color, child));
+    }
+}
