4fd9c2f81106ef7b7d153a9280851cff8121d02a
Gorash <chm@odoo.com>
2020-05-26 21:02:49 +0200

    [IMP] Char: improve CharNode renderer performances

--- packages/plugin-char/src/CharHtmlDomRenderer.ts
+++ packages/plugin-char/src/CharHtmlDomRenderer.ts
@@ -2,6 +2,7 @@ import { AbstractRenderer } from '../../plugin-renderer/src/AbstractRenderer';
 import { CharNode } from './CharNode';
 import { InlineNode } from '../../plugin-inline/src/InlineNode';
 import { HtmlDomRenderingEngine } from '../../plugin-html/src/HtmlDomRenderingEngine';
+import { VNode } from '../../core/src/VNodes/VNode';
 
 export class CharHtmlDomRenderer extends AbstractRenderer<Node[]> {
     static id = HtmlDomRenderingEngine.id;
@@ -10,19 +11,19 @@ export class CharHtmlDomRenderer extends AbstractRenderer<Node[]> {
     async render(node: CharNode): Promise<Node[]> {
         // Consecutive compatible char nodes are rendered as a single text node.
         let text = '' + node.char;
-        let next = node.nextSibling();
         const charNodes = [node];
-        while (next && node.isSameTextNode(next)) {
-            if (next instanceof CharNode) {
-                charNodes.push(next);
-                if (next.char === ' ' && text[text.length - 1] === ' ') {
-                    // Browsers don't render consecutive space chars otherwise.
-                    text += '\u00A0';
-                } else {
-                    text += next.char;
-                }
+        const siblings = node.parent.children();
+        let index = siblings.indexOf(node) + 1;
+        let next: VNode;
+        while ((next = siblings[index]) && node.isSameTextNode(next)) {
+            charNodes.push(next);
+            if (next.char === ' ' && text[text.length - 1] === ' ') {
+                // Browsers don't render consecutive space chars otherwise.
+                text += '\u00A0';
+            } else {
+                text += next.char;
             }
-            next = next.nextSibling();
+            index++;
         }
         // Render block edge spaces as non-breakable space (otherwise browsers
         // won't render them).
