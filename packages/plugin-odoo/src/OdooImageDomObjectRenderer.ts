166b2af99f4143134ed13e5970851a5a499a752c
Antoine Guenet <age@odoo.com>
2020-06-08 15:29:48 +0200

    odoo image html renderer to dom object renderer

---
+++ packages/plugin-odoo/src/OdooImageDomObjectRenderer.ts
@@ -0,0 +1,22 @@
+import { ImageDomObjectRenderer } from '../../plugin-image/src/ImageDomObjectRenderer';
+import { ImageNode } from '../../plugin-image/src/ImageNode';
+import { DomObject } from '../../plugin-html/src/DomObjectRenderingEngine';
+
+export class OdooImageDomObjectRenderer extends ImageDomObjectRenderer {
+    predicate = ImageNode;
+
+    async render(node: ImageNode): Promise<DomObject> {
+        const image = await this.super.render(node);
+        if (image && 'tag' in image) {
+            const savedAttach = image.attach;
+            image.attach = (el: HTMLElement): void => {
+                savedAttach(el);
+                el.addEventListener('dblclick', () => {
+                    const params = { image: node };
+                    this.engine.editor.execCommand('openMedia', params);
+                });
+            };
+        }
+        return image;
+    }
+}
