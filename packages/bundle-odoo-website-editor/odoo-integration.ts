aebacd31fd283abab469723d43a95834e4e362b1
Antoine Guenet <age@odoo.com>
2020-06-09 17:03:28 +0200

    remove addClassToLink

--- packages/bundle-odoo-website-editor/odoo-integration.ts
+++ packages/bundle-odoo-website-editor/odoo-integration.ts
@@ -6,6 +6,9 @@ import { DomLayoutEngine } from '../plugin-dom-layout/src/ui/DomLayoutEngine';
 import { Layout } from '../plugin-layout/src/Layout';
 import { Renderer } from '../plugin-renderer/src/Renderer';
 import { ImageNode } from '../plugin-image/src/ImageNode';
+import { InlineNode } from '../plugin-inline/src/InlineNode';
+import { LinkFormat } from '../plugin-link/src/LinkFormat';
+import { Attributes } from '../plugin-xml/src/Attributes';
 import { getOdooCommands } from '../plugin-odoo/src/OdooCommands';
 
 export {
@@ -19,4 +22,7 @@ export {
     withRange,
     VRange,
     getOdooCommands,
+    InlineNode,
+    LinkFormat,
+    Attributes,
 };
