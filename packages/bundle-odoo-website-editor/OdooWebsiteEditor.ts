2a8581050ea694498e740fe3c1472442a7093d71
Nicolas Bayet <nicolas.bayet@gmail.com>
2020-06-19 14:33:19 +0200

    [FIX] OdooWebsiteEditor: allow the main zone to stretch vertically

--- packages/bundle-odoo-website-editor/OdooWebsiteEditor.ts
+++ packages/bundle-odoo-website-editor/OdooWebsiteEditor.ts
@@ -138,13 +138,13 @@ export class OdooWebsiteEditor extends JWEditor {
         const defaultTemplate = `
         <t-dialog><t t-zone="default"/></t-dialog>
         <div class="wrap_editor d-flex flex-column">
-            <div class="d-flex flex-row overflow-auto">
+            <div class="d-flex flex-grow-1 flex-row overflow-auto">
                 <t t-zone="main_sidebar"/>
                 <div class="d-flex flex-column overflow-auto o_editor_center">
                     <div class="o_toolbar">
                         <t t-zone="tools"/>
                     </div>
-                    <div class="d-flex overflow-auto">
+                    <div class="d-flex flex-grow-1 overflow-auto">
                         <t t-zone="snippetManipulators"/>
                         <t t-zone="main"/>
                     </div>
