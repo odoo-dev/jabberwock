30ec1a8a2c0c19634347f7c63749b726ff3c021c
Nicolas Bayet <nicolas.bayet@gmail.com>
2020-06-23 14:18:18 +0200

    [FIX] odoo image binding

--- packages/plugin-odoo/src/OdooImageDomObjectRenderer.ts
+++ packages/plugin-odoo/src/OdooImageDomObjectRenderer.ts
@@ -9,14 +9,23 @@ export class OdooImageDomObjectRenderer extends ImageDomObjectRenderer {
         const image = await this.super.render(node);
         if (image && 'tag' in image) {
             const savedAttach = image.attach;
+            const savedDetach = image.detach;
+            const handleClick = (): void => {
+                const params = { image: node };
+                this.engine.editor.execCommand('openMedia', params);
+            };
             image.attach = (el: HTMLElement): void => {
                 if (savedAttach) {
                     savedAttach(el);
                 }
-                el.addEventListener('dblclick', () => {
-                    const params = { image: node };
-                    this.engine.editor.execCommand('openMedia', params);
-                });
+                el.addEventListener('dblclick', handleClick);
+            };
+
+            image.detach = (el: HTMLElement): void => {
+                if (savedDetach) {
+                    savedDetach(el);
+                }
+                el.removeEventListener('dblclick', handleClick);
             };
         }
         return image;
