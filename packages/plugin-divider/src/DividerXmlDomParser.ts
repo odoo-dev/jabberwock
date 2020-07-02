35011bfac523c38e4047ff64f768d6663cf04935
Antoine Guenet <age@odoo.com>
2020-07-02 10:23:57 +0200

    [REF] Divider: replace divider configuration with mode

--- packages/plugin-divider/src/DividerXmlDomParser.ts
+++ packages/plugin-divider/src/DividerXmlDomParser.ts
@@ -2,7 +2,6 @@ import { AbstractParser } from '../../plugin-parser/src/AbstractParser';
 import { XmlDomParsingEngine } from '../../plugin-xml/src/XmlDomParsingEngine';
 import { DividerNode } from './DividerNode';
 import { nodeName } from '../../utils/src/utils';
-import { Divider } from './Divider';
 
 export class DividerXmlDomParser extends AbstractParser<Node> {
     static id = XmlDomParsingEngine.id;
@@ -15,9 +14,6 @@ export class DividerXmlDomParser extends AbstractParser<Node> {
     async parse(item: Element): Promise<DividerNode[]> {
         const divider = new DividerNode();
         divider.modifiers.append(this.engine.parseAttributes(item));
-
-        // `DividerNode` can be configured to be unbreakable.
-        divider.breakable = this.engine.editor.plugins.get(Divider).configuration.breakable;
 
         const nodes = await this.engine.parse(...item.childNodes);
         divider.append(...nodes);
