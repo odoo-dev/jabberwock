7269a7e6af9bea00b9d7668418b3bf7579791e55
Antoine Guenet <age@odoo.com>
2020-07-02 11:57:35 +0200

    [ADD] ContextManager: get ranked matches
    Co-authored-by: Nicolas Bayet <nby@odoo.com>
    Co-authored-by: Antoine Guenet <age@odoo.com>

--- packages/core/src/ContextManager.ts
+++ packages/core/src/ContextManager.ts
@@ -18,6 +18,32 @@ export interface Contextual {
     check?: (context: CheckingContext) => boolean;
 }
 
+export interface ContextualEntry<T> extends Contextual {
+    selector: Predicate[];
+    value: T;
+}
+
+export interface RankedResult<T> {
+    lvl1Score: number;
+    lvl2Score: number;
+    index: number;
+    matched: VNode[];
+    entry: ContextualEntry<T>;
+}
+
+/**
+ * Class that rank a hierarchy of vnode through a "specificity" algorithm.
+ *
+ * Specificity level is defined with:
+ * - `lvl2`: if the last predicate of an item selector is deeper in the tree
+ *   than the last predicate of another item selector; the first item have
+ *   more specificity
+ * - `lvl1`: if two or more items have the same `lvl2` specificity; the
+ *   command with the longest selector will have more specificity
+ * - `lvl0`: if the item has no selector (an empty list), there is no
+ *   specificity
+ * ```
+ */
 export class ContextManager {
     editor: JWEditor;
     defaultContext: Context;
@@ -34,138 +60,111 @@ export class ContextManager {
     //--------------------------------------------------------------------------
 
     /**
-     * Match a command from the current context (the `vDocument` selection).
-     *
-     * Search through all command with `commandId` and return the last
-     * `CommandImplementation` that match a particular context.
-     *
-     * Commands with more specificity will have priority.  If there is multiples
-     * commands with the same specificity, the last defined will be retured.
-     *
-     * The context is the `editor.selection.range.start.ancestors()`.
-     *
-     * Specificity is defined with:
-     * - `lvl2`: if the last predicate of a command "a"'s selector is deeper in
-     *   the tree than the last predicate of a command "b"'s selector; command
-     *   "a" have more specificity
-     * - `lvl1`: if two or more commands have the same `lvl2` specificity; the
-     *   command with the longest selector will have more specificity
-     * - `lvl0`: if the command has no selector, there is no specificity
-     *
-     * For example:
-     * ```typescript
-     * const commandUlP: CommandImplementation = {
-     *     selector: [isUl, isP],
-     *     handler: () =>{},
-     * }
-     * const commandP: CommandImplementation = {
-     *     selector: [isP],
-     *     handler: () =>{},
-     * }
-     * const commandUl: CommandImplementation = {
-     *     selector: [isUl],
-     *     handler: () =>{},
-     * }
-     * const commandImage: CommandImplementation = {
-     *     selector: [isImage],
-     *     handler: () =>{},
-     * }
-     * const commandAny: CommandImplementation = {
-     *     selector: [],
-     *     handler: () =>{},
-     * }
-     * dispatcher.registerCommand('command', commandUlP)
-     * dispatcher.registerCommand('command', commandP)
-     * dispatcher.registerCommand('command', commandUl)
-     * dispatcher.registerCommand('command', commandImage)
-     * dispatcher.registerCommand('command', commandAny)
-     * const result = dispatcher._mach('command')
-     * ```
-     *
-     * If the document looks like:
-     * ```html
-     * <ul>
-     *     <li>
-     *         <!-- The char "[]" represent the collapsed selection -->
-     *         <p>[]a</p>
-     *     </li>
-     * </ul>
-     * ```
-     *
-     * The ancestors list is `['ul', 'li', 'p']`.
-     *
-     * The 4 command `commandUlP`, `commandP`, `commandUl`, `commandAny` could
-     * match the `command` identifier but not `commandImage`.
-     *
-     * The priority is calculated with "`lvl2`,`lvl1`".
-     * - `commandUlP` specificity: 2,2 `lvl2`: 2 means the last predicate
-     *   (`isP`) is found at depth 2 in ancestors `lvl1`: 2 means there are 2
-     *   predicates in `commandUlP`'s selector
-     * - `commandP` specificity: 2,1 `lvl2`: 2 means the last predicate (`isP`)
-     *   is found at depth 2 in ancestors `lvl1`: 1 means there is 1 predicate
-     *   in `commandP`'s selector
-     * - `commandUl` specificity: 0,1 `lvl0`: 0 means the last predicate
-     *   (`isUl`) is found at depth 0 in ancestors `lvl1`: 1 means there is 1
-     *   predicate in `commandP`'s selector
-     * - `commandAny` specificity: -1,0 No selector means no specificity.
-     *   `lvl0`: -1 means it has no depth.  `lvl1`: 0 means there is 0
-     *   predicates in `commandAny`'s selector
-     *
-     * The result will be `commandUlP` because it has the highest specificity.
+     * Test all contextuals against a hierarchy of VNodes and return the result
+     * ordered by specificity.
+     */
+    static getRankedMatches<T>(
+        hierarchy: VNode[],
+        contextuals: ContextualEntry<T>[],
+    ): RankedResult<T>[] {
+        const matches: RankedResult<T>[] = [];
+
+        for (let index = 0; index < contextuals.length; index++) {
+            const contextual = contextuals[index];
+            const match = ContextManager._matchNodes(hierarchy, contextual.selector);
+
+            if (match) {
+                matches.push({
+                    lvl1Score: match[0],
+                    lvl2Score: match[1].length,
+                    matched: match[1],
+                    index: index,
+                    entry: contextual,
+                });
+            }
+        }
+
+        // Sort the matches:
+        // - from highest to lowest score
+        // - when the score is the same, from highest to lowest index
+        const rankedMatch = matches.sort(function(a, b) {
+            if (b.lvl1Score === a.lvl1Score && b.lvl2Score === a.lvl2Score) {
+                return b.index - a.index;
+            } else if (b.lvl1Score === a.lvl1Score) {
+                return b.lvl2Score - a.lvl2Score;
+            } else {
+                return b.lvl1Score - a.lvl1Score;
+            }
+        });
+        return rankedMatch;
+    }
+    /**
+     * Match items selector depending on the editor current context range and
+     * return the most specific item.
      *
      * @param items
      * @param paramsContext
      */
     match<T extends Contextual>(items: T[], paramsContext?: Context): [T, Context] {
-        let maxFirstMatchDepth = -1;
-        let maxSelectorLength = 0;
-        let matchingItem: T;
-        let matchingContext: Context;
-
-        for (const item of items) {
-            const context = { ...this.defaultContext, ...item.context, ...paramsContext };
-            let firstMatchDepth = -1;
-            let index = 0;
-            const selector = item.selector || [];
-            const matches: VNode[] = [];
-            const start = context.range.start;
-            const nodes = start.ancestors();
-            const node = start.previousSibling() || start.nextSibling();
-            if (node) {
-                nodes.unshift(node);
-            }
-            const maximumDepth = nodes.length - 1;
-            for (const predicate of [...selector].reverse()) {
-                let matchFound = false;
-                while (!matchFound && index < nodes.length) {
-                    if (nodes[index].test(predicate)) {
-                        matchFound = true;
-                        matches.unshift(nodes[index]);
-                        if (firstMatchDepth === -1) {
-                            // Deeper match has higher specificity. So lower
-                            // index in ancestors, means higher specificity.
-                            firstMatchDepth = maximumDepth - index;
-                        }
-                    }
-                    index++;
-                }
-                // Stop checking the predicates of this particular command
-                // since at least one of them don't match the context.
-                if (!matchFound) break;
-            }
-
-            if (
-                matches.length === selector.length &&
-                maxFirstMatchDepth <= firstMatchDepth &&
-                maxSelectorLength <= selector.length &&
-                (!item.check || item.check({ ...context, selector: matches }))
-            ) {
-                maxFirstMatchDepth = firstMatchDepth;
-                maxSelectorLength = selector.length;
-                matchingItem = item;
-                matchingContext = context;
-            }
+        const context = { ...this.defaultContext, ...paramsContext };
+        const start = context.range.start;
+        const hierarchy = start.ancestors();
+        const node = start.previousSibling() || start.nextSibling();
+        if (node) {
+            hierarchy.unshift(node);
         }
-        return [matchingItem, matchingContext];
+        const entries: ContextualEntry<T>[] = items.map(item => {
+            const entry: ContextualEntry<T> = {
+                selector: item.selector || [],
+                value: item,
+            };
+            return entry;
+        });
+
+        const matches = ContextManager.getRankedMatches(hierarchy, entries);
+        const match = matches.find(match => {
+            return (
+                !match.entry.value.check ||
+                match.entry.value.check({ ...context, selector: match.matched })
+            );
+        });
+        return [match?.entry.value, context];
+    }
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * Check whether a hierarchy of `VNode` match with `selector`.
+     *
+     * Return a tuple with the first value being level of specificity lvl2 and
+     * the second value being the VNnode that matched with a selector
+     * (specificity lvl1).
+     */
+    static _matchNodes(hierarchy: VNode[], selector: Predicate[]): [number, VNode[]] | undefined {
+        const matches: VNode[] = [];
+        const maximumDepth = hierarchy.length - 1;
+        let firstMatchDepth = -1;
+        let index = 0;
+        for (const predicate of [...selector].reverse()) {
+            let matchFound = false;
+            while (!matchFound && index < hierarchy.length) {
+                if (hierarchy[index].test(predicate)) {
+                    matchFound = true;
+                    matches.unshift(hierarchy[index]);
+                    if (firstMatchDepth === -1) {
+                        // Deeper match has higher specificity. So lower
+                        // index in ancestors, means higher specificity.
+                        firstMatchDepth = maximumDepth - index;
+                    }
+                }
+                index++;
+            }
+            // Stop checking the predicates of this particular command
+            // since at least one of them don't match the context.
+            if (!matchFound) break;
+        }
+        return matches.length === selector.length && [firstMatchDepth, matches];
     }
 }
