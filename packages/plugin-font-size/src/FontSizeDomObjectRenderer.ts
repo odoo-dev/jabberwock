e1f119c3cb26f91ed44bce7bc79cb4a9f6166dcd
Nicolas Bayet <nicolas.bayet@gmail.com>
2020-08-04 16:32:15 +0200

    [IMP] FontSize: change font size

---
+++ packages/plugin-font-size/src/FontSizeDomObjectRenderer.ts
@@ -0,0 +1,43 @@
+import { InputDomObjectRenderer } from './../../plugin-input/src/InputDomObjectRenderer';
+import { InputNode } from './../../plugin-input/src/InputNode';
+import { CharNode } from '../../plugin-char/src/CharNode';
+import { VNode } from '../../core/src/VNodes/VNode';
+import { DomObjectRenderingEngine } from '../../plugin-renderer-dom-object/src/DomObjectRenderingEngine';
+import { Attributes } from '../../plugin-xml/src/Attributes';
+import { Layout } from '../../plugin-layout/src/Layout';
+import { DomLayoutEngine } from '../../plugin-dom-layout/src/DomLayoutEngine';
+
+export class FontSizeDomObjectRenderer extends InputDomObjectRenderer {
+    static id = DomObjectRenderingEngine.id;
+    engine: DomObjectRenderingEngine;
+    predicate = (node: VNode): boolean =>
+        node instanceof InputNode && node.inputName === 'font-size';
+
+    /**
+     * @override
+     */
+    _onEditorChange(node: InputNode, input: HTMLInputElement): void {
+        super._onEditorChange(node, input);
+        const range = this.engine.editor.selection.range;
+        const fontNodes = range.selectedNodes(CharNode);
+
+        const fontSize =
+            fontNodes[0] && fontNodes[0].modifiers.get(Attributes).style.get('font-size');
+
+        input.style.display = 'block';
+        if (fontSize) {
+            input.value = parseInt(fontSize).toString();
+        } else if (fontNodes[0]) {
+            const layout = this.engine.editor.plugins.get(Layout);
+            const domLayout = layout.engines.dom as DomLayoutEngine;
+            const firstDomNode = domLayout.getDomNodes(fontNodes[0])[0];
+            const firstDomElement =
+                firstDomNode instanceof HTMLElement ? firstDomNode : firstDomNode.parentElement;
+            const style = getComputedStyle(firstDomElement);
+            input.value = parseInt(style['font-size']).toString();
+        } else {
+            input.value = '';
+            input.style.display = 'none';
+        }
+    }
+}
