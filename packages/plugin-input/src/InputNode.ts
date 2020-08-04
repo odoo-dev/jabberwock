f0026031618a6042abb229cc7572dad77df7940c
Nicolas Bayet <nicolas.bayet@gmail.com>
2020-08-04 16:44:30 +0200

    [IMP] Input: add binding to input

--- packages/plugin-input/src/InputNode.ts
+++ packages/plugin-input/src/InputNode.ts
@@ -1,13 +1,25 @@
 import { VElement } from '../../core/src/VNodes/VElement';
+import JWEditor from '../../core/src/JWEditor';
+
+export interface InputNodeParams {
+    inputType?: string;
+    inputName?: string;
+    value?: string;
+    change?: (editor: JWEditor) => void;
+}
 
 export class InputNode extends VElement {
     value: string;
     inputName: string;
     inputType: string;
-    constructor(params: { type?: string; name?: string; value?: string } = {}) {
+    constructor(params: InputNodeParams = {}) {
         super({ htmlTag: 'INPUT' });
-        this.inputName = params.name;
-        this.inputType = params.type;
-        this.value = params.value;
+        this.inputName = params.inputName || '';
+        this.inputType = params.inputType || 'text';
+        this.value = params.value || '';
+        if (params.change) this.change = params.change;
     }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-empty-function
+    change(editor: JWEditor): void {}
 }
