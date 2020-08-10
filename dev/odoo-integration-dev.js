ecf170b4c9db6c262ad8ee78ba4e5a452b16c2b5
Nicolas Bayet <nicolas.bayet@gmail.com>
2020-08-10 12:54:18 +0200

    [IMP] Odoo integration: make default port 8080

--- dev/odoo-integration-dev.js
+++ dev/odoo-integration-dev.js
@@ -4,7 +4,7 @@ odoo.define('web_editor.jabberwock', function(require) {
 
     const script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = 'http://localhost:8095/odoo-integration.js';
+    script.src = 'http://localhost:8080/odoo-integration.js';
     document.getElementsByTagName('head')[0].appendChild(script);
     return new Promise(resolve => {
         script.onload = () => {
