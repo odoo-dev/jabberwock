87fcbe6e63805462b9c9e9e6449cfc8e0a25a164
Antoine Guenet <age@odoo.com>
2020-08-10 11:01:01 +0200

    [FIX] Table: prevent rendering a cell outside of a table

--- packages/plugin-table/src/TableCellDomObjectRenderer.ts
+++ packages/plugin-table/src/TableCellDomObjectRenderer.ts
@@ -7,6 +7,7 @@ import {
 } from '../../plugin-renderer-dom-object/src/DomObjectRenderingEngine';
 import { VRange } from '../../core/src/VRange';
 import { Table } from './Table';
+import { TableNode } from './TableNode';
 
 export class TableCellDomObjectRenderer extends NodeRenderer<DomObject> {
     static id = DomObjectRenderingEngine.id;
@@ -16,7 +17,10 @@ export class TableCellDomObjectRenderer extends NodeRenderer<DomObject> {
     async render(cell: TableCellNode): Promise<DomObject> {
         // If the cell is not active, do not render it (it means it is
         // represented by its manager cell's colspan or rowspan: it was merged).
-        if (!cell.isActive()) return { children: [] };
+        // TODO: remove `TableNode` check: it's a temporary fix for the memory
+        // system, which should not try to render the cell if it's not in the
+        // VDocument.
+        if (!cell.isActive() || !cell.ancestor(TableNode)) return { children: [] };
 
         // Render the cell and its contents.
         const domObject: DomObject = {
