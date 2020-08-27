30ad68517f1dcb88d05890bb5e1283e2c5e68930
Sébastien Geelen (sge) <sge@odoo.com>
2020-08-27 10:40:25 +0200

    [IMP] FontAwesome : Select the icon automaticaly when user click

--- packages/plugin-fontawesome/src/FontAwesomeDomObjectRenderer.ts
+++ packages/plugin-fontawesome/src/FontAwesomeDomObjectRenderer.ts
@@ -20,10 +20,27 @@ export class FontAwesomeDomObjectRenderer extends NodeRenderer<DomObject> {
         node: FontAwesomeNode,
         worker: RenderingEngineWorker<DomObject>,
     ): Promise<DomObject> {
+        const select = (ev: Event) => {
+            ev.preventDefault();
+            const selectFontAwesome = () => {
+                this.engine.editor.selection.select(node, node);
+            };
+            // The normaliser wait a tick before changing the selection when the user click
+            // in order to override the normalizer default behavior we also need to wait a tick
+            setTimeout(() => {
+                this.engine.editor.execCommand(selectFontAwesome);
+            });
+        };
         const fontawesome: DomObjectElement = {
             tag: node.htmlTag,
             attributes: {
                 class: new Set(node.faClasses),
+            },
+            attach: (el: HTMLElement): void => {
+                el.addEventListener('mouseup', select, true);
+            },
+            detach: (el: HTMLElement): void => {
+                el.removeEventListener('mouseup', select, true);
             },
         };
         // Surround the fontawesome with two invisible characters so the
