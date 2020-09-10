43ef611fb66705b14359642a4910615274b94817
Nicolas Bayet <nicolas.bayet@gmail.com>
2020-09-10 17:03:18 +0200

    [FIX] FontAwesome: remove params from fontawesome
    We need to retrieve the fontawesome at the time of the command
    openMedia. Which is done in Odoo code.

--- packages/plugin-odoo/src/OdooFontAwesomeDomObjectRenderer.ts
+++ packages/plugin-odoo/src/OdooFontAwesomeDomObjectRenderer.ts
@@ -2,7 +2,6 @@ import { FontAwesomeNode } from '../../plugin-fontawesome/src/FontAwesomeNode';
 import { DomObject } from '../../plugin-renderer-dom-object/src/DomObjectRenderingEngine';
 import { FontAwesomeDomObjectRenderer } from '../../plugin-fontawesome/src/FontAwesomeDomObjectRenderer';
 import { RenderingEngineWorker } from '../../plugin-renderer/src/RenderingEngineCache';
-import { RelativePosition } from '../../core/src/VNodes/VNode';
 
 export class OdooFontAwesomeDomObjectRenderer extends FontAwesomeDomObjectRenderer {
     predicate = FontAwesomeNode;
@@ -17,20 +16,7 @@ export class OdooFontAwesomeDomObjectRenderer extends FontAwesomeDomObjectRender
 
             if ('tag' in fa) {
                 const dbclickCallback = () => {
-                    const faZoomClassRegex = RegExp('fa-[0-9]x');
-                    function filterUnwantedClasses(className) {
-                        return !className.startsWith('fa') || faZoomClassRegex.test(className);
-                    }
-                    // Customize the look and behavior of the media modal when
-                    // the origin of the media modal is a FA icon double click
-                    const params = {
-                        noImages: true,
-                        noDocuments: true,
-                        noVideos: true,
-                        htmlClass: [...fa.attributes.class].filter(filterUnwantedClasses).join(' '),
-                    };
-
-                    this.engine.editor.execCommand('openMedia', params);
+                    this.engine.editor.execCommand('openMedia', {});
                 };
 
                 const savedAttach = fa.attach;
