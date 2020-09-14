ace4915b97d23755ac16c328e944a680143ac0d2
Nicolas Bayet <nicolas.bayet@gmail.com>
2020-09-14 18:03:35 +0200

    [IMP] Odoo: add information when change
    Add info to OdooField and OdooStructure to be retrieved by Odoo to know
    if the field has changed.

--- packages/plugin-odoo/src/OdooStructureNode.ts
+++ packages/plugin-odoo/src/OdooStructureNode.ts
@@ -8,10 +8,20 @@ export class OdooStructureNode extends TagNode {
     xpath: string;
     viewId: string;
     breakable = false;
+    dirty = false;
 
     constructor(params: OdooStructureNodeParams) {
         super(params);
         this.xpath = params.xpath;
         this.viewId = params.viewId;
+        // Don't trigger the first change as it comes from parsing.
+        let firstChange = false;
+        this.on('childList', () => {
+            if (!firstChange) {
+                firstChange = true;
+                return;
+            }
+            this.dirty = true;
+        });
     }
 }
