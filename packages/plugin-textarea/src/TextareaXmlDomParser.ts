ee61f777eaa5de9438d980631ba2c6185186a8d9
Emilien Durieu (edu) <edu@odoo.com>
2020-11-26 10:49:58 +0100

    [FIX] Textarea: use tag name for textarea parser predicate
    Textarea were not parsed correctly when parsed as xml, which happened when loading existing page from the backend.

--- packages/plugin-textarea/src/TextareaXmlDomParser.ts
+++ packages/plugin-textarea/src/TextareaXmlDomParser.ts
@@ -1,12 +1,13 @@
 import { VNode } from '../../core/src/VNodes/VNode';
 import { AbstractParser } from '../../plugin-parser/src/AbstractParser';
 import { XmlDomParsingEngine } from '../../plugin-xml/src/XmlDomParsingEngine';
+import { nodeName } from '../../utils/src/utils';
 import { TextareaNode } from './TextareaNode';
 
 export class TextareaXmlDomParser extends AbstractParser<Node> {
     static id = XmlDomParsingEngine.id;
     engine: XmlDomParsingEngine;
-    predicate = (item: Node): boolean => item instanceof HTMLTextAreaElement;
+    predicate = (item: Node): boolean => nodeName(item) === 'TEXTAREA';
 
     /**
      * Parse a list (UL, OL) and its children list elements (LI).
