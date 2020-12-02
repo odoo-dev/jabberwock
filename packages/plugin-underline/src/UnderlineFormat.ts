e0414f79f873c8759baa2e4a91a05980ba9e00ad
Sébastien Geelen (sge) <sge@odoo.com>
2020-12-02 16:31:27 +0100

    [FIX] Underline: always put <u> as deep as possible in the rendered DOM
    The underline style (`text-decoration: underline;`) can be broken if a child element has a css `display: inline-block;`. ( see W3C spec https://www.w3.org/TR/CSS21/text.html#lining-striking-props ).
    To combat this we have ton ensure that a `<u>` tag is as deep a possible in the rendered DOM, so he cannot contain other child element expect Characters.
    
    + Add unit tests

--- packages/plugin-underline/src/UnderlineFormat.ts
+++ packages/plugin-underline/src/UnderlineFormat.ts
@@ -1,5 +1,7 @@
 import { Format } from '../../core/src/Format';
+import { ModifierLevel } from '../../core/src/Modifier';
 
 export class UnderlineFormat extends Format {
     htmlTag = 'U';
+    level = ModifierLevel.LOW;
 }
