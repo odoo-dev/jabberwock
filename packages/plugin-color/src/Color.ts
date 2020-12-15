0e18338af5d6e0c2719bd3de8c12b974dcc49fd0
Emilien Durieu (edu) <edu@odoo.com>
2020-12-15 11:35:38 +0100

    [FIX] Color, TextColor: fix background color behavior
    It turns out that the color behavior was not a standard behavior but specific to TextColor (at least in the context of Odoo).
    The simpler behavior is now implemented in `Color` and is used by `BackgroundColor` while the specific behavior is implemented in `TextColor` directly.

--- packages/plugin-color/src/Color.ts
+++ packages/plugin-color/src/Color.ts
@@ -1,6 +1,5 @@
 import { JWPlugin, JWPluginConfig } from '../../core/src/JWPlugin';
 import { CommandParams } from '../../core/src/Dispatcher';
-import { Inline } from '../../plugin-inline/src/Inline';
 import { VNode } from '../../core/src/VNodes/VNode';
 import { AbstractNode } from '../../core/src/VNodes/AbstractNode';
 import { InlineNode } from '../../plugin-inline/src/InlineNode';
@@ -8,7 +7,7 @@ import { Format } from '../../core/src/Format';
 import { Attributes } from '../../plugin-xml/src/Attributes';
 import { CssStyle } from '../../plugin-xml/src/CssStyle';
 import { Modifiers } from '../../core/src/Modifiers';
-import { ZoneNode } from '../../plugin-layout/src/ZoneNode';
+import { CharNode } from '../../plugin-char/src/CharNode';
 
 export interface ColorParams extends CommandParams {
     color?: string; // css-valid color name
@@ -63,40 +62,9 @@ export class Color<T extends ColorConfig = ColorConfig> extends JWPlugin<T> {
                 [this.styleName]: color,
             });
         } else {
-            let selectedNodes = range.selectedNodes(node => !(node instanceof ZoneNode));
-            selectedNodes = selectedNodes.filter(node => !selectedNodes.includes(node.parent));
-            // Color the highest ancestor.
+            const selectedNodes = range.traversedNodes(CharNode);
             for (const node of selectedNodes) {
-                // Color the highest fully selected format if any.
-                const fullySelectedFormats = this._newFormats(node).filter(format => {
-                    // This format is started by this node. Now find out if we
-                    // end it within the selection.
-                    return selectedNodes.includes(this._lastNodeWithFormat(node, format));
-                });
-                if (fullySelectedFormats.length) {
-                    const highestFullFormat = fullySelectedFormats.pop();
-                    const pairs: [VNode | Format, Format][] = selectedNodes.map(selectedNode => {
-                        const format = this._findFormat(selectedNode, highestFullFormat);
-                        if (format) {
-                            return [this._inheritsColorFrom(selectedNode, color), format];
-                        }
-                    });
-                    for (const pair of pairs) {
-                        if (pair?.[1]) {
-                            // Color the formats.
-                            if (pair[0]) {
-                                // If the node inherited the color, remove the
-                                // inherited color.
-                                this._removeColor(pair[0]);
-                            }
-                            this._applyColor(pair[1], color);
-                        }
-                    }
-                } else if (!this._inheritsColorFrom(node, color)) {
-                    // Skip if the node already has the right color, through an
-                    // ancestor or a format.
-                    this._applyColor(node, color);
-                }
+                this._applyColor(node, color);
             }
         }
     }
@@ -123,27 +91,10 @@ export class Color<T extends ColorConfig = ColorConfig> extends JWPlugin<T> {
                 range.modifiers?.find(Attributes).style.remove(this.styleName);
             }
         } else {
-            for (const node of params.context.range.selectedNodes()) {
+            for (const node of params.context.range.traversedNodes(CharNode)) {
                 const target = this._nodeOrFirstFormat(node);
-                const currentColor = target.modifiers.find(Attributes)?.style.get(this.styleName);
-
-                if (!currentColor || currentColor === defaultColor || node.ancestor(hasColor)) {
-                    // Set the color to the default color.
-                    target.modifiers.get(Attributes).style.set(this.styleName, defaultColor);
-                } else {
-                    // Remove the color.
-                    target.modifiers.find(Attributes)?.style.remove(this.styleName);
-                }
-
-                // Uncolor the children and their formats as well.
-                for (const child of node.children()) {
-                    child.modifiers.find(Attributes)?.style.remove(this.styleName);
-                    if (child instanceof InlineNode) {
-                        for (const format of child.modifiers.filter(Format)) {
-                            format.modifiers.find(Attributes)?.style.remove(this.styleName);
-                        }
-                    }
-                }
+                // Remove the color.
+                target.modifiers.find(Attributes)?.style.remove(this.styleName);
             }
         }
     }
@@ -169,69 +120,7 @@ export class Color<T extends ColorConfig = ColorConfig> extends JWPlugin<T> {
     _isAllColored(node: VNode, color: string): boolean {
         return node.children().every(child => this.hasColor(color, child));
     }
-    /**
-     * Return the first format that matches the given format, on the given node.
-     *
-     * @param node
-     * @param format
-     */
-    private _findFormat(node: VNode, format: Format): Format {
-        return node.modifiers.filter(Format).find(nodeFormat => nodeFormat.isSameAs(format));
-    }
-    /**
-     * Return the last consecutive node to have the given format (assumed to be
-     * held by the given node too).
-     *
-     * @param node
-     * @param format
-     */
-    private _lastNodeWithFormat(node: VNode, format: Format): VNode {
-        let current = node;
-        let next = node.nextSibling();
-        while (this._findFormat(next, format)) {
-            current = next;
-            next = current.nextSibling();
-        }
-        return current;
-    }
-    /**
-     * Return all formats that are started by the given node.
-     *
-     * @param node
-     */
-    private _newFormats(node: VNode): Format[] {
-        const formats = node.modifiers.filter(Format);
-        const previous = node.previousSibling();
-        // A new format is starting if the previous sibling doesn't have it.
-        if (!previous) return formats;
-        return formats.filter(format => !this._findFormat(previous, format));
-    }
-    /**
-     * If the given node inherits the given color through an ancestor of a
-     * format, or if it simply has it itself, return the node or format it
-     * inherits it from.
-     *
-     * @param node
-     * @param color
-     */
-    private _inheritsColorFrom(node: VNode, color: string): VNode | Format | undefined {
-        if (this.hasColor(color, node)) {
-            return node;
-        }
-        const colorAncestor = node.ancestor(this.hasColor.bind(this));
-        if (colorAncestor && this.hasColor(color, colorAncestor)) {
-            return colorAncestor;
-        }
-        for (const format of node.modifiers.filter(Format)) {
-            if (this.hasColor(color, format)) {
-                return format;
-            }
-        }
-    }
-    private _applyColor(node: VNode | Format, color: string): void {
+    protected _applyColor(node: VNode | Format, color: string): void {
         node.modifiers.get(Attributes).style.set(this.styleName, color);
-    }
-    private _removeColor(node: VNode | Format): void {
-        node.modifiers.get(Attributes).style.remove(this.styleName);
     }
 }
